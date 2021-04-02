905ddfa9814cf915dd720294875dddc832685ad7
Sujal Lama <lsujal41@gmail.com>
2021-04-02 10:55:03 +0545

    messages

---
+++ src/Components/MessageDropdown/MessageDropdown.js
@@ -0,0 +1,13 @@
+import './style.css';
+import {Link} from 'react-router-dom';
+import React from 'react'
+
+const MessageDropdown = ({active}) => {
+    return (
+        <div className="message-dropdown">
+            this is messageDropdown
+        </div>
+    )
+}
+
+export default MessageDropdown
