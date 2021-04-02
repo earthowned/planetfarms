b2dfeb385a38c5ba2da3f49d3d02b492e337bda3
Sujal Lama <lsujal41@gmail.com>
2021-04-02 10:55:03 +0545

    messages

--- src/Components/Header/HeaderComponent.js
+++ src/Components/Header/HeaderComponent.js
@@ -1,15 +1,19 @@
-import React from 'react'
+import {useState} from 'react'
+import MessageDropdown from '../MessageDropdown/MessageDropdown'
 import './style.css'
 
 const HeaderComponent = () => {
+    const [active, setActive] = useState(false);
     return (
         <>
         <header className="header-container">
             <h3>Messenger</h3>
             <ul>
                 <li><img src="./img/person.png" /><span>My dashboard</span></li>
-                <li className="message"><img src="./img/message.png" />
-                <span className="message-noti">6</span>
+                <li onClick={() => setActive(true)} className="message"><img src="./img/message.png" />
+                {active || <div className="message-noti">6
+                </div>}
+                {active && <MessageDropdown />}
                 </li>
                 <li><img src="./img/bell.png" /></li>
                 <li><img src="./img/user.png" /></li>
