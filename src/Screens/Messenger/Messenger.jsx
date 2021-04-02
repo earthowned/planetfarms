e325d196f9c4b1283978e1e8d90e5056f5250d82
Laxman Maharjan <email@mlaxman.com.np>
2021-04-02 19:40:25 +0545

    style change

--- src/Screens/Messenger/Messenger.jsx
+++ src/Screens/Messenger/Messenger.jsx
@@ -2,7 +2,7 @@ import React,{useState} from 'react'
 import './style.css';
 import HeaderComponent from '../../Components/Header/HeaderComponent';
 import MessengerMain from '../../Components/MessengerMain/MessengerMain';
-import DashboardLayout from './Layout/DashboardLayout/DashboardLayout';
+import DashboardLayout from '../../Layout/DashboardLayout/DashboardLayout';
 
 
 const Messenger = () => {
