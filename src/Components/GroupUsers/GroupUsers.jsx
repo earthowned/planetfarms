81b47c42543783d4e10fac99386bf21f15408d62
sujal <71555740+SujalLama@users.noreply.github.com>
2021-04-07 22:40:39 +0545

    library screens (#15)

--- src/Components/GroupUsers/GroupUsers.jsx
+++ src/Components/GroupUsers/GroupUsers.jsx
@@ -1,40 +1,19 @@
 import React from 'react'
 import './group-users.css'
 
-const groupUsers = [
-    {
-        image: "./img/user.png",
-        name: "Jane Cooper",
-        active: false,
-    },
-    {
-        image: "./img/user2.png",
-        name: "Ronald Richards",
-        active: true,
-    },
-    {
-        image: "./img/user-2.png",
-        name: "Janny Wilson",
-        active: true,
-    },
-    {
-        image: "./img/user.png",
-        name: "Cody Fisher",
-        active: false,
-    },
-]
-const GroupUsers = () => {
+
+const GroupUsers = ({data}) => {
     
     return (
         <>
         {
-           groupUsers.map(user => (
+           data.map(item => (
             <div className="group-user-container">
                 <div className="img-wrapper">
-                    <img src={user.image} />
+                    <img src={item.image} />
                 </div>
-                <h4>{user.name}</h4>
-                <input type="radio" checked={user.active} className="radio-default" />
+                <h4>{item.name}</h4>
+                <input type="radio" checked={item.active} className="radio-default" />
             </div>
            )) 
             }
