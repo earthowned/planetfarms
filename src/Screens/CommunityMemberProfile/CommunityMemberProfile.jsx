33670f6fe829a803f6ec473464a5b4d76677010d
Dixit Sigdel <dixitsigdel8@gmail.com>
2021-04-08 09:43:36 +0545

    refactoring function in communit-member-pfd

--- src/Screens/CommunityMemberProfile/CommunityMemberProfile.jsx
+++ src/Screens/CommunityMemberProfile/CommunityMemberProfile.jsx
@@ -11,70 +11,46 @@ function App() {
     </DashboardLayout>
   );
 }
-
 export default App;
+
+const DataField=({title,first_inputTitle,first_inputValue,second_inputTitle,second_inputValue,third_inputTitle,third_inputValue})=>{
+  return(
+  <div className="x-information">
+     <div className="flex-row-4">
+       <div className="flex-col-5">
+        <div className="contact-information ibmplexsans-semi-bold-quarter-spanish-white-16px"> {title} </div>
+        <div className="email-container">
+           <div className="email-text ibmplexsans-regular-normal-monsoon-14px"> {first_inputTitle}</div>
+           <div className="email-address ibmplexsans-semi-bold-quarter-spanish-white-16px">  {first_inputValue} </div>
+         </div>
+     </div>
+       <div className="phone-container">
+         <div className="phone-text ibmplexsans-regular-normal-monsoon-14px">{second_inputTitle}</div>
+         <div className="phone-number ibmplexsans-semi-bold-quarter-spanish-white-16px"> {second_inputValue} </div>
+       </div>
+       <div className="dob">
+       <div className="date-of-birhday-text ibmplexsans-regular-normal-monsoon-14px">{third_inputTitle} </div>
+       <div className="date-text ibmplexsans-semi-bold-quarter-spanish-white-16px">{third_inputValue} </div>
+     </div>
+     </div>
+     <div className="border-line"></div>
+   </div>
+  )
+}
 
 const PersonalInformation=()=>{
   return(
-    <div className="x-information">
-    <div className="flex-row-5">
-    <div className="flex-col-8">
-      <div className="personal-information-text ibmplexsans-semi-bold-quarter-spanish-white-16px"> Personal infromation</div>
-      <div className="first-name-input">
-        <div className="first-name ibmplexsans-regular-normal-monsoon-14px"> First name</div>
-        <div className="name ibmplexsans-semi-bold-quarter-spanish-white-16px"> Mikhail</div>
-      </div>
-    </div>
-    <div className="last-name">
-      <div className="first-name ibmplexsans-regular-normal-monsoon-14px">Last Name </div>
-      <div className="name ibmplexsans-semi-bold-quarter-spanish-white-16px">  Ugryum</div>
-    </div>
-    <div className="dob">
-      <div className="date-of-birhday-text ibmplexsans-regular-normal-monsoon-14px">Date of birhday </div>
-      <div className="date-text ibmplexsans-semi-bold-quarter-spanish-white-16px"> 08/23/1995</div>
-    </div>
-  </div>
-  <div className="border-line"></div>
-  </div>
+  <DataField title={"Personal infromation"} first_inputTitle={"First name"}  first_inputValue={"Mikhail"}  second_inputTitle={"Last Name"} second_inputValue={"Ugryum"} third_inputTitle={"Date of birhday"} third_inputValue={"08/23/1995"}   />
   )
 }
-
 const ContactInformation=()=>{
   return(
-    <div className="x-information">
-    <div className="flex-row-4">
-      <div className="flex-col-5">
-        <div className="contact-information ibmplexsans-semi-bold-quarter-spanish-white-16px"> Contact information </div>
-        <div className="email-container">
-          <div className="email-text ibmplexsans-regular-normal-monsoon-14px"> Email</div>
-          <div className="email-address ibmplexsans-semi-bold-quarter-spanish-white-16px">  mikhail@gmail.pro </div>
-        </div>
-      </div>
-      <div className="phone-container">
-        <div className="phone-text ibmplexsans-regular-normal-monsoon-14px">Phone</div>
-        <div className="phone-number ibmplexsans-semi-bold-quarter-spanish-white-16px"> +1 (233) 369-50-15 </div>
-      </div>
-    </div>
-    <div className="border-line"></div>
-  </div>
+  <DataField title={"Contact information"} first_inputTitle={"Email"}  first_inputValue={"mikhail@gmail.pro"}  second_inputTitle={"Phone"} second_inputValue={"+1 (233) 369-50-15"}  />
   )
 }
-
 const AdditionalInformation=()=>{
   return(
-    <div className="additional-information-container">
-    <div className="flex-col-7">
-      <div className="additional-information-text ibmplexsans-semi-bold-quarter-spanish-white-16px">Additional information </div>
-      <div className="additional-information-login">
-        <div className="last-login ibmplexsans-regular-normal-monsoon-14px"> Last login </div>
-        <div className="login-date ibmplexsans-semi-bold-quarter-spanish-white-16px"> 09/22/2020  </div>
-      </div>
-    </div>
-    <div className="frame-2824">
-      <div className="numbers-of-visits ibmplexsans-regular-normal-monsoon-14px"> Numbers of visits</div>
-      <div className="number-5 ibmplexsans-semi-bold-quarter-spanish-white-16px"> 153</div>
-    </div>
-  </div>
+  <DataField title={"Additional information"} first_inputTitle={"Last login"}  first_inputValue={"09/22/2020 "}  second_inputTitle={"Numbers of visits"} second_inputValue={"153"}    />
   )
 }
 
@@ -94,7 +70,6 @@ const ProfileImage=()=>{
       </div>
     </div>
   </div>
-
   )
 }
 
