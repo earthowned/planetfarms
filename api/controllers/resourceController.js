4b117648d8a031316722ded3134c24931c2d7219
robinGiri <girirobin2018@gmail.com>
2021-04-09 18:05:02 +0545

    get individual resource done

--- api/controllers/resourceController.js
+++ api/controllers/resourceController.js
@@ -1,6 +1,4 @@
-
-
-const Resource = require('../models/resourceModel.js');
+const Resource = require("../models/resourceModel.js");
 
 // @desc    Fetch all resources
 // @route   GET /api/resources
@@ -10,20 +8,62 @@ const paginate = ({ page, pageSize }) => {
   const limit = offset + pageSize;
 
   return {
-      offset,
-      limit
+    offset,
+    limit
   };
 };
 
-const getResources = (req, res) =>{
-  const pageSize = 10
-  const page = Number(req.query.pageNumber) || 1
+const getResources = (req, res) => {
+  const pageSize = 10;
+  const page = Number(req.query.pageNumber) || 1;
 
-  Resource.findAll({ offset: page, limit: pageSize }).then(resources => {
-    paginate({page, pageSize});
-    res.json({ resources, page, pageSize }).status(200);
-  })
-  .catch(err => res.json({err}).status(400));
+  Resource.findAll({ offset: page, limit: pageSize })
+    .then(resources => {
+      paginate({ page, pageSize });
+      res.json({ resources, page, pageSize }).status(200);
+    })
+    .catch((err) => res.json({ err }).status(400));
 };
 
-module.exports = getResources; 
+// @desc    Add individual resource
+// @route   POST /api/resources/add
+// @access  Public
+const addResource = (req, res) => {
+  const {
+    title, author, year, description, tag, language, publisher, linkToLicense, subject, level, mediaType, resourceFor, openWith, resourceType, isDownloadable, attachments,
+  } = req.body;
+
+  Resource.create({
+    title, author, year, description, tag, language, publisher, linkToLicense, subject, level, mediaType, resourceFor, openWith, resourceType, isDownloadable, attachments,
+  })
+  .then(() => res.json({ message:'Resource Created !!!' }).status(200))
+  .catch((err) => res.json( { error: err.message }).status(400));
+};
+
+// @desc    Fetch single resource
+// @route   GET /api/resource/:id
+// @access  Public
+const getResourcesById = (req, res) => {
+  const id = req.params.id;
+
+  Resource.findByPk(id)
+  .then((product) => {
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404);
+      throw new Error('Resource not found');
+    }
+  })
+.catch((err) => res.json( { error: err.message }).status(400));
+};
+
+const deleteResources = (req, res) => {
+
+};
+
+const updateResources = (req, res) => {
+
+};
+
+module.exports = { getResources, addResource, getResourcesById, deleteResources, updateResources };
--- api/routes/resourceRouter.js
+++ api/routes/resourceRouter.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const getResources = require('../controllers/resourceController.js');
+const { getResources, addResource, getResourcesById, deleteResources, updateResources } = require('../controllers/resourceController.js');
 
 router.route('/').get(getResources); 
+router.route('/add').post(addResource); 
+router
+  .route('/:id')
+  .get(getResourcesById)
+  .delete(deleteResources)
+  .put(updateResources)
 
 module.exports = router;
