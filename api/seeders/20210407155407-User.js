c7e7e57185c38ea6ae32f98353a41502c52da1a0
Robin Giri <45058288+robinGiri@users.noreply.github.com>
2021-04-09 09:22:06 +0545

    node express and postgresql (fixes #9) (#23)
    Co-authored-by: Laxman Maharjan <email@mlaxman.com.np>

---
+++ api/seeders/20210407155407-User.js
@@ -0,0 +1,24 @@
+
+const faker = require('faker');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    let data = [];
+    let amount = 20;
+    let date = new Date();
+
+    while(amount--) {
+      data.push({
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+        createdAt: date,
+        updatedAt: date,
+      });
+    }
+    return queryInterface.bulkInsert('users', data, {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('users', null, {});
+  }
+};
