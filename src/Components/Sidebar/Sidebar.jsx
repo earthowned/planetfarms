9fdf85abbd5c0a95d0d026016c0b699e1611eb40
dixitsigdel8 <72726217+dixitsigdel8@users.noreply.github.com>
2021-04-09 21:40:59 +0545

    better sidebar css (fixes #12) (#32)
    Co-authored-by: Laxman Maharjan <email@mlaxman.com.np>

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,44 +1,82 @@
-import React,{useState} from "react";
-import { Link, useHistory } from "react-router-dom";
+import { useState } from "react";
+import { Link, useHistory, useLocation } from "react-router-dom";
 import "./sidebar.css";
 
 const Sidebar = ({ setToggle, toggle }) => {
   let history = useHistory();
   const [dropdownActive, setDropdownActive] = useState(true);
+  let {pathname}=useLocation()
+  const [activeBar,setActiveBar]=useState()
+
+  const handleOnClick=()=>{
+      setDropdownActive(!dropdownActive)
+  }
+
+  const handleToggle=()=>{
+    setToggle(!toggle)
+    if (toggle === true) {
+        setDropdownActive(false)
+    } else {
+        setDropdownActive(true)
+    }
+  }
+  
   return (
     <>
-      <button className="nav-icon" onClick={() => setToggle(!toggle)}>
-        <img src="/img/sidebar-arrow-icon.svg" alt="arrow-icon" />{" "}
+      <button className="nav-icon" onClick={()=>handleToggle()}>
+        <img src="/img/nav-icon.png" />
       </button>
       <div className="logo-container">
         <div className="img-container">
-          <img src="/img/logo.svg" alt="logo" />
+          <img src="/img/logo.png" />
         </div>
         <div className="img-container-2">
-          <img src="/img/grid-icon.svg" alt="grid-icon" />
+          <img src="/img/square-icon.png" />
         </div>
       </div>
+
       <ul className="list-container">
-        <li
-          className="list-items"
-          onClick={() => history.push("/community-page-news")}
-        >
-          <img src="/img/user-management.svg" alt="user-management" />{" "}
-          <span>Ragarians</span>
-          <ul className="dropdown-container">
-            <li className="dropdown-item">Members</li>
-            <li className="dropdown-item">Groups</li>
-            <li className="dropdown-item">Enterprises</li>
-          </ul>
+        <li className="list-items"> 
+          <div onClick={handleOnClick} className={`${pathname === '/community-page-news' ? " text-menu text-active" :"text-menu" }`}>
+            <div  onClick={() => history.push("/community-page-news")} className="align-content">   
+             <img src="/img/rag-icon.svg" /> <span >Ragarians</span>
+            </div>
+          </div>
+          {dropdownActive  && (
+            <ul className="dropdown-container">
+              <div   className={`${pathname === '/community-members' ? "  text-active" :"" }`}>
+                <li onClick={()=>history.push('/community-members')} className="dropdown-item">
+                  <strong>Members</strong>
+                </li>
+              </div>        
+              <div className={`${pathname === '/community-groups' ? "  text-active" :"" }`}>
+                <li className="dropdown-item">
+                  <strong> Groups</strong>
+                </li>
+              </div>
+              <div  className={`${pathname === '/community-enterprises' ? "  text-active" :"" }`}>
+                <li className="dropdown-item">
+                  <strong>Enterprises</strong>
+                </li>
+              </div>
+            </ul>
+          )}
         </li>
-        <li className="list-items">
-          <Link className="nav-link" to="/library">
-            <img src="/img/book.svg" alt="book-icon" /> <span>Library</span>
-          </Link>
+
+        <li className=" list-items-menu">      
+          <div onClick={()=>history.push('/library')}  className={`${pathname === '/library' ? "text-menu text-active" : "text-menu"}`}>
+            <div className="align-content">
+              <img src="/img/book.png" /> <span>Library</span>
+            </div>
+          </div>
         </li>
-        <li className="list-items">
-          <img src="/img/graduation-cap-1.svg" alt="graduation-icon" />{" "}
-          <span>Course</span>
+        
+        <li className="list-items-menu">
+          <div className={`${pathname === '/course' ? "text-menu text-active" : "text-menu"}`}>
+            <div className="align-content">
+              <img src="/img/graduation.png" /> <span>Course</span>
+            </div>
+          </div>
         </li>
       </ul>
     </>
