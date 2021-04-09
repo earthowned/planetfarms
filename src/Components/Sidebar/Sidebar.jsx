c9e9d3afb906f800f22b82c6fcb7e924b1be4df5
Laxman Maharjan <email@mlaxman.com.np>
2021-04-09 21:54:45 +0545

    update icons on sidebar (#43)

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -24,14 +24,14 @@ const Sidebar = ({ setToggle, toggle }) => {
   return (
     <>
       <button className="nav-icon" onClick={()=>handleToggle()}>
-        <img src="/img/nav-icon.png" />
+        <img src="/img/sidebar-arrow-icon.svg" />
       </button>
       <div className="logo-container">
         <div className="img-container">
-          <img src="/img/logo.png" />
+          <img src="/img/logo.svg" />
         </div>
         <div className="img-container-2">
-          <img src="/img/square-icon.png" />
+          <img src="/img/grid-icon.png" />
         </div>
       </div>
 
@@ -66,7 +66,7 @@ const Sidebar = ({ setToggle, toggle }) => {
         <li className=" list-items-menu">      
           <div onClick={()=>history.push('/library')}  className={`${pathname === '/library' ? "text-menu text-active" : "text-menu"}`}>
             <div className="align-content">
-              <img src="/img/book.png" /> <span>Library</span>
+              <img src="/img/book.svg" /> <span>Library</span>
             </div>
           </div>
         </li>
@@ -74,7 +74,7 @@ const Sidebar = ({ setToggle, toggle }) => {
         <li className="list-items-menu">
           <div className={`${pathname === '/course' ? "text-menu text-active" : "text-menu"}`}>
             <div className="align-content">
-              <img src="/img/graduation.png" /> <span>Course</span>
+              <img src="/img/graduation-cap-1.svg" /> <span>Course</span>
             </div>
           </div>
         </li>
