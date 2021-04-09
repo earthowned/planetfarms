77677dbe9ab3a4c8565203c7792ef9bf881857dc
Laxman Maharjan <email@mlaxman.com.np>
2021-04-09 08:17:59 +0545

    back button

--- src/Components/BackButton/BackButton.jsx
+++ src/Components/BackButton/BackButton.jsx
@@ -6,13 +6,11 @@ import { Link } from "react-router-dom";
 const BackButton = ({ location }) => {
   return (
     <Link to={location}>
-      <div className="back-btn">
-        <div className="icons-arrows-left">
-          <div className="overlap-group">
-            <img className="vector-stroke" alt="back" src="/img/back-button-icon.svg" />
-          </div>
+      <div className="back-btn-container">
+        <div className="icon-wrapper">
+          <img className="icon-image-btn" src="/img/back-button-icon.svg" />
         </div>
-        <div className="place valign-text-middle ibmplexsans-semi-bold-quarter-spanish-white-16px">Back</div>
+        <div className="back-text">Back</div>
       </div>
     </Link>
   )
