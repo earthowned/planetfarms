f40420340988685f054cad1cd4e4c9246d1c13a6
robinGiri <girirobin2018@gmail.com>
2021-04-13 15:12:02 +0545

    lint fix

--- api/controllers/courseController.js
+++ api/controllers/courseController.js
@@ -1,34 +1,33 @@
-const Courses = require("../models/courseModel.js");
-const path = require('path');
-const Sequelize = require('sequelize');
-const Op = Sequelize.Op;
+const Courses = require('../models/courseModel.js')
+const path = require('path')
+const Sequelize = require('sequelize')
+const Op = Sequelize.Op
 
 // @desc    Fetch all course
 // @route   GET /api/courses
 // @access  Public
 const paginate = ({ page, pageSize }) => {
-  const offset = page * pageSize;
-  const limit = offset + pageSize;
+  const offset = page * pageSize
+  const limit = offset + pageSize
 
   return {
     offset,
     limit
-  };
-};
+  }
+}
 
 const getCourses = (req, res) => {
-  const pageSize = 10;
-  const page = Number(req.query.pageNumber) || 1;
-  const order = req.query.order || 'ASC';
+  const pageSize = 10
+  const page = Number(req.query.pageNumber) || 1
+  const order = req.query.order || 'ASC'
 
   Courses.findAll({ offset: page, limit: pageSize, order: [['title', order]] })
     .then(courses => {
-      paginate({ page, pageSize });
-      res.json({ courses, page, pageSize }).status(200);
+      paginate({ page, pageSize })
+      res.json({ courses, page, pageSize }).status(200)
     })
-    .catch((err) => res.json({ err }).status(400));
-};
-
+    .catch((err) => res.json({ err }).status(400))
+}
 
 // @desc    Add individual course
 // @route   POST /api/courses/add
@@ -36,15 +35,15 @@ const getCourses = (req, res) => {
 const addCourse = (req, res) => {
   const {
     title, description, languageOfInstruction, memberLimit, method, gradeLevel, subjectLevel, creator, steps
-  } = req.body;
-  const __dirname = path.resolve(path.dirname(''));
+  } = req.body
+  const __dirname = path.resolve(path.dirname(''))
   Courses.create({
-    _attachments: path.join(path.dirname(__dirname), "/uploads" + req.file.filename),
+    _attachments: path.join(path.dirname(__dirname), '/uploads' + req.file.filename),
     title, description, languageOfInstruction, memberLimit, method, gradeLevel, subjectLevel, creator, steps
   })
   .then(() => res.json({ message:'Course Created !!!' }).status(200))
-  .catch((err) => res.json( { error: err.message }).status(400));
-};
+  .catch((err) => res.json( { error: err.message }).status(400))
+}
 
 // @desc    Update a course
 // @route   PUT /api/courses/:id
@@ -52,69 +51,69 @@ const addCourse = (req, res) => {
 const updateCourse = (req, res) => {
   const {
     title, description, languageOfInstruction, memberLimit, method, gradeLevel, subjectLevel, creator, steps
-  } = req.body;
-  const id = req.params.id;
+  } = req.body
+  const id = req.params.id
   Courses.findByPk(id).then(product => {
     if (product) {
-      const { id } = product;
+      const { id } = product
       Courses.update({
         _attachments: path.join(path.dirname(__dirname), "/uploads" + req.file.filename),
         title, description, languageOfInstruction, memberLimit, method, gradeLevel, subjectLevel, creator, steps
       },
         { where: { id } })
         .then(() => res.json({ message:'Course Updated !!!' }).status(200))
-        .catch((err) => res.json( { error: err.message }).status(400));
+        .catch((err) => res.json( { error: err.message }).status(400))
       }
       res.status(404)
       throw new Error('Course not found')
-  });
-};
+  })
+}
 
 // @desc    Fetch single course
 // @route   GET /api/courses/:id
 // @access  Public
 const getCourseById = (req, res) => {
-  const id = req.params.id;
+  const id = req.params.id
 
   Courses.findByPk(id)
   .then(course => {
     if (course) {
-      res.json(course);
+      res.json(course)
     } else {
-      res.status(404);
-      throw new Error('Course not found');
+      res.status(404)
+      throw new Error('Course not found')
     }
   })
-.catch((err) => res.json( { error: err.message }).status(400));
-};
+.catch((err) => res.json( { error: err.message }).status(400))
+}
 
 // @desc    Delete a course
 // @route   delete /api/courses/:id
 // @access  Public
 const deleteCourse = (req, res) => {
-  const id = req.params.id;
+  const id = req.params.id
   Courses.findByPk(id).then(resource => {
-    if (resource){
-      const { id } = resource;
-      Courses.destroy({ where: {id}})
-      .then(() => res.json({ message:'Course Deleted !!!' }).status(200))
-      .catch((err) => res.json( { error: err.message }).status(400));
+    if (resource) {
+      const { id } = resource
+      Courses.destroy({ where: { id } })
+        .then(() => res.json({ message:'Course Deleted !!!' }).status(200))
+        .catch((err) => res.json({ error: err.message }).status(400))
     } else {
-      res.status(404);
-      throw new Error('Course not found');
+      res.status(404)
+      throw new Error('Course not found')
     }
   })
-};
+}
 
 // @desc    Search title
 // @route   POST /api/courses/search
 // @access  Private
 const searchCoursesTitle = (req, res) => {
-  let { title } = req.query;
+  const { title } = req.query
 
   Courses.findAll({ where: { title: { [Op.like]: '%' + title + '%' } } })
     .then(title => res.json({ title }).status(200))
-    .catch(err => res.json({ error: err }).status(400));
-};
+    .catch(err => res.json({ error: err }).status(400))
+}
 
-module.exports = { addCourse, getCourses, updateCourse, getCourseById, deleteCourse, searchCoursesTitle };
+module.exports = { addCourse, getCourses, updateCourse, getCourseById, deleteCourse, searchCoursesTitle }
--- api/routes/courseRouter.js
+++ api/routes/courseRouter.js
@@ -1,26 +1,26 @@
-const path = require('path');
-const express = require('express');
-const multer = require('multer');
-const router = express.Router();
-const shortid = require('shortid');
-const { getCourses, addCourse, updateCourse, getCourseById, deleteCourse, searchCoursesTitle } = require('../controllers/courseController.js');
+const path = require('path')
+const express = require('express')
+const multer = require('multer')
+const router = express.Router()
+const shortid = require('shortid')
+const { getCourses, addCourse, updateCourse, getCourseById, deleteCourse, searchCoursesTitle } = require('../controllers/courseController.js')
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-     cb(null, path.join(path.dirname(__dirname), "/uploads"));
+    cb(null, path.join(path.dirname(__dirname), '/uploads'))
   },
   filename: function (req, file, cb) {
-    cb(null, shortid.generate() + "-" + file.originalname);
-  },
-});
+    cb(null, shortid.generate() + "-" + file.originalname)
+  }
+})
 
 function checkFileType(file, cb) {
   const filetypes = /jpg|jpeg|png/
-  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-  const mimetype = filetypes.test(file.mimetype);
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase())
+  const mimetype = filetypes.test(file.mimetype)
 
   if (extname && mimetype) {
-    return cb(null, true);
+    return cb(null, true)
   } else {
     cb('Images only!')
   }
@@ -29,20 +29,19 @@ function checkFileType(file, cb) {
 const upload = multer({
   storage,
   fileFilter: function (req, file, cb) {
-    checkFileType(file, cb);
-  },
-});
+    checkFileType(file, cb)
+  }
+})
 
-router.route('/').get(getCourses); 
+router.route('/').get(getCourses) 
 
 // for upload we have just worked with images jpg|jpeg|png for other types of file we need to work.
-router.route('/add').post(upload.single('avatar'), addCourse);
-router.route('/search').get(searchCoursesTitle);
+router.route('/add').post(upload.single('avatar'), addCourse)
+router.route('/search').get(searchCoursesTitle)
 router
   .route('/:id')
   .get(getCourseById)
   .delete(deleteCourse)
-  .put(updateCourse);
+  .put(updateCourse)
 
- module.exports = router;
- 
+module.exports = router
