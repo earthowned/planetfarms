b2a44c52141463664a7d862e41dc3658963efb2d
robinGiri <girirobin2018@gmail.com>
2021-04-13 16:38:02 +0545

    router lint fix

--- api/routes/resourceRouter.js
+++ api/routes/resourceRouter.js
@@ -2,8 +2,8 @@ const express = require('express')
 const router = express.Router()
 const { getResources, addResource, getResourcesById, deleteResources, updateResources, searchResourcesTitle } = require('../controllers/resourceController.js')
 
-router.route('/').get(getResources) 
-router.route('/add').post(addResource) 
+router.route('/').get(getResources)
+router.route('/add').post(addResource)
 router.route('/search').get(searchResourcesTitle)
 router
   .route('/:id')
