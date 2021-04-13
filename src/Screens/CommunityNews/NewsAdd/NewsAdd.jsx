53d69a725309d6928db13814800acc93c4737a10
sujal <71555740+SujalLama@users.noreply.github.com>
2021-04-13 13:23:56 +0545

    add files functionality improved (fixes #33) (#35)
    Co-authored-by: Laxman Maharjan <email@mlaxman.com.np>

--- src/Screens/CommunityNews/NewsAdd/NewsAdd.jsx
+++ src/Screens/CommunityNews/NewsAdd/NewsAdd.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import NewsCreateModal from '../../../Components/NewsCreateModal/NewsCreateModal';
 import DashboardLayout from '../../../Layout/DashboardLayout/DashboardLayout'
 import {useParams} from 'react-router-dom';
@@ -12,22 +12,39 @@ const NewsAdd = () => {
      const [videoActive, setVideoActive] = useState(true);
     const [imageActive, setImageActive] = useState(true);
     const [textActive, setTextActive] = useState(true);
+    const [content, setContent] = useState({
+        title: ""
+    })
+    const {id} = useParams();
 
+    useEffect(() => {
+        setContent({
+        title: id,
+        image: "",
+        videoTitle: "",
+        videoDesc: "",
+        textTitle: "",
+        textDesc: "",
+    })
+    }, [])
+    
     return (
         <>
-        {createVideoModal && <NewsCreateModal type="video" videoActive={videoActive} setVideoActive={setVideoActive}/>}
-        {createImageModal && <NewsCreateModal type="image" imageActive={imageActive} setImageActive={setImageActive} />}
-        {createTextModal && <NewsCreateModal type="text" textActive={textActive} setTextActive={setTextActive}/>}
+        {createVideoModal && <NewsCreateModal setContent={setContent} content={content} type="video" videoActive={videoActive} setVideoActive={setVideoActive}/>}
+        {createImageModal && <NewsCreateModal setContent={setContent} type="image" imageActive={imageActive} setImageActive={setImageActive} />}
+        {createTextModal && <NewsCreateModal setContent={setContent} type="text" textActive={textActive} setTextActive={setTextActive}/>}
            <DashboardLayout title="Add News">
             {/* back button comes here */}
             <NewsAddMainContainer setCreateVideoModal={setCreateVideoModal} setCreateImageModal={setCreateImageModal}
-            setCreateTextModal={setCreateTextModal} setTextActive={setTextActive} setImageActive={setImageActive} setVideoActive={setVideoActive}/>
+            setCreateTextModal={setCreateTextModal} setTextActive={setTextActive} setImageActive={setImageActive} setVideoActive={setVideoActive}
+            content={content}
+            />
             </DashboardLayout>
         </>
     )
 }
 
-function NewsAddMainContainer ({setCreateVideoModal, setCreateImageModal, setCreateTextModal, setVideoActive, setImageActive, setTextActive}) {
+function NewsAddMainContainer ({setCreateVideoModal, setCreateImageModal, setCreateTextModal, setVideoActive, setImageActive, setTextActive, content}) {
 
     function createVideo () {
         setCreateVideoModal(true);
@@ -53,24 +70,36 @@ function NewsAddMainContainer ({setCreateVideoModal, setCreateImageModal, setCre
     
     return(
         <div className="news-add-main-container">
-            <NewsAddContainer createVideo={createVideo} createImage={createImage} createText={createText}/>  
+            <NewsAddContainer content={content} createVideo={createVideo} createImage={createImage} createText={createText}/>  
             <PopUp />
          </div>
     )
 }
 
-function NewsAddContainer ({createVideo, createImage, createText}) {
-    const {id} = useParams();
-    console.log(id);
+function NewsAddContainer ({createVideo, createImage, createText, content}) {
     return(
          <div className="news-add-container">
-             <h4>{id}</h4>
-                <div className="news-add-inner-container">
-                    <button onClick={() => createVideo()} className="add-btn"><img src="/img/video-outline.svg" /> <span>Add video</span></button>
+             <NewContent content={content} />
+             <div className="news-add-inner-container">
+                    <button onClick={() => createVideo()} className="add-btn" ><img src="/img/video-outline.svg" /> <span>Add video</span></button>
                     <button onClick={() => createImage()} className="add-btn"><img src="/img/camera-outline.svg" /> <span>Add image</span></button>
                     <button onClick={() => createText()} className="add-btn"><img src="/img/file-text-outline.svg" /><span>Add text</span></button>
                 </div>
         </div>
+    )
+}
+
+function NewContent({content}) {
+    return (
+        <>
+        <div className="new-content-container">
+            <h2>{content.title}</h2>
+            {content.image && <img src={content.image} alt="new-image" />}
+            {content.videoTitle && <h6>{content.videoTitle}</h6>}
+            {content.textTitle && <h4>{content.textTitle}</h4>}
+            {content.textDesc && <p>{content.textDesc}</p>}
+        </div>
+        </>
     )
 }
 
