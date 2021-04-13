9373b4635ada21c7bad546d1cce086ffb6bb8ddb
Laxman Maharjan <email@mlaxman.com.np>
2021-04-13 21:39:36 +0545

    linting fix in serverjs

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ const app = express()
 const userRoutes = require('./routes/userRouter.js')
 const resourceRoutes = require('./routes/resourceRouter.js')
 const courseRoutes = require('./routes/courseRouter.js')
-const sequelize = require('./config/database.js');
+const sequelize = require('./config/database.js')
 
 const PORT = process.env.port || 5000
 
@@ -21,7 +21,7 @@ app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API on planetfarms' })
 })
 
-// port connection 
+// port connection
 app.listen(PORT, () => {
   console.log(`PlanetFarms is running on PORT ${PORT}.`)
 })
