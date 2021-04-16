37d7ebc50d7024eb6fdbf7129e16b13a21e3d23e
sujal <71555740+SujalLama@users.noreply.github.com>
2021-04-16 22:01:19 +0545

    signup logo adjust (fixes #49)(fixes #63) (#62)
    Co-authored-by: Laxman Maharjan <email@mlaxman.com.np>

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 import "./button.css"
 
 const Button = ({name, clickHandler}) => {
+  function submitForm(e){
+    e.preventDefault();
+    clickHandler(e)
+  }
     return (
-            <button className="btn-default" onClick={(e) => clickHandler(e)}>
+            <button type="submit" className="btn-default" onClick={(e) => submitForm(e)}>
                 <div
                   type="submit"
                   className="btn-text"
