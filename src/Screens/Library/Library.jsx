c80f3a95c52e2463fd66e712e113112ba11292b2
Robin Giri <45058288+robinGiri@users.noreply.github.com>
2021-04-16 11:37:53 +0545

    initial library listing from db (#60)

--- src/Screens/Library/Library.jsx
+++ src/Screens/Library/Library.jsx
@@ -1,21 +1,30 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import LibraryHeader from '../../Components/LibraryHeader/LibraryHeader'
 import './library.css'
-import DashboardLayout from '../../Layout/DashboardLayout/DashboardLayout';
-import ListView from '../../Components/ListView/ListView';
-import SimpleModal from '../../Components/SimpleModal/SimpleModal';
-import CollectionModal from '../../Components/CollectionModal/CollectionModal';
-import GroupModal from '../../Components/GroupModal/GroupModal';
-import {groupCollection, articles, videos} from './CollectionData'
+import DashboardLayout from '../../Layout/DashboardLayout/DashboardLayout'
+import ListView from '../../Components/ListView/ListView'
+import SimpleModal from '../../Components/SimpleModal/SimpleModal'
+import CollectionModal from '../../Components/CollectionModal/CollectionModal'
+import GroupModal from '../../Components/GroupModal/GroupModal'
+import {groupCollection, videos} from './CollectionData'
+import { useDispatch, useSelector } from 'react-redux'
+import { listResources } from '../../actions/resourceActions'
 
 const Library = () => {
-    const [newCollection, setNewCollection] = useState(false);
-    const [active, setActive] = useState(false);
-    const [modalActive, setModalActive] = useState(false);
+    const dispatch = useDispatch()
+    const resourceList = useSelector((state) => state.listResources)
+    const { resources } = resourceList
+    useEffect(() => {
+        dispatch(listResources('', ''))
+    }, [dispatch])
+
+    const [newCollection, setNewCollection] = useState(false)
+    const [active, setActive] = useState(false)
+    const [modalActive, setModalActive] = useState(false)
 
     function openAddCollection () {
-        setModalActive(true);
-        setActive(false);
+        setModalActive(true)
+        setActive(false)
     }
     
     return (
@@ -33,7 +42,7 @@ const Library = () => {
                
            <LibraryHeader setActive={setActive}/>
            <div className="list-container">
-           <ListView title="Articles" data={articles} 
+           <ListView title="Articles" data={resources} 
            setNewCollection={setNewCollection}
            modalActive={modalActive}
            setModalActive={setModalActive}
--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import "./style.css"
 import "./index.css"
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App'
+import reportWebVitals from './reportWebVitals'
 import './index.css'
+import store from './store'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
