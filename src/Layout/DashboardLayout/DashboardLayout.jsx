3dd41463d83a11447c27fc23abc68b32e306b9fb
sujal <71555740+SujalLama@users.noreply.github.com>
2021-04-16 13:30:24 +0545

    responsive community news page (fixes #45)(fixes #48) (#55)
    Co-authored-by: Laxman Maharjan <email@mlaxman.com.np>
    Co-authored-by: dogi <dogi@users.noreply.github.com>

--- src/Layout/DashboardLayout/DashboardLayout.jsx
+++ src/Layout/DashboardLayout/DashboardLayout.jsx
@@ -1,12 +1,29 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import HeaderComponent from '../../Components/Header/HeaderComponent'
 import Sidebar from '../../Components/Sidebar/Sidebar'
+import "./dashboard.css";
 
 const DashboardLayout = ({title, children}) => {
     const [toggle, setToggle] = useState(true);
+    
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  function calculateWidth () {
+    setWindowWidth(window.innerWidth);
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", calculateWidth);
+
+    return () => {
+      window.removeEventListener("resize",calculateWidth);
+    }
+  },[calculateWidth])
+
     return (
         <div>
-           <div className="container">
+          {
+              windowWidth > 1200 ? <div className="container">
             <div className={toggle ? "sidebar-container" : "sidebar-container active"}>
                 <Sidebar setToggle={setToggle} toggle={toggle}/>
             </div>
@@ -14,8 +31,27 @@ const DashboardLayout = ({title, children}) => {
                 <HeaderComponent title={title}/>
                 {children}
             </div> 
+            </div> : (<>
+            <div className="layout-container">
+                <div className="dashboard-header">
+                    <img src="/img/p-icon.svg" />
+                    <img src="/img/grid-icon.svg" />
+                    <img src="/img/mobile-user.svg" />
+                </div>
+                <h1>{title}</h1>
+                <div >{children}</div>
+                <div className="space-taker"></div>
             </div>
-            </div>
+            <div className="footer-nav">
+                    <img src="/img/user-green-outline.svg" />
+                    <img src="/img/email-outline.svg" />
+                    <img src="/img/bell-icon.svg" alt="bell icon"/>
+                    <img src="/img/ham-menu.svg" alt="ham menu"/>
+                </div>
+                </>
+            )
+          } 
+        </div>
     )
 }
 
