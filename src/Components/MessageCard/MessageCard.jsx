a0e467ee7aef538afed4bfdc4565769ce93bd173
Sujal Lama <lsujal41@gmail.com>
2021-04-22 12:43:25 +0545

    call-modal-responsive

--- src/Components/MessageCard/MessageCard.jsx
+++ src/Components/MessageCard/MessageCard.jsx
@@ -7,19 +7,18 @@ const MessageCard = ({contact, clickHandler}) => {
     const windowWidth = useSizeFinder();
     return (
         <button className="contact-card" key={contact.name} onClick={() => windowWidth > 850 ? clickHandler(true) : history.push(`/messenger/${contact.name}`)}>
-                           <div className="img-wrapper">
-                           <img src={contact.img} />
-                           </div>
-                           <div className="contact-text">
-                               <h4>{contact.name}</h4>
-                               <p>{contact.message}</p>
-                           </div>
-                           <div className="contact-noti">
-                               <span>{contact.date}</span>
-                               <div className="noti">2</div>
-                           </div>
-         </button>
-
+                <div className="img-wrapper">
+                    <img src={contact.img} />
+                </div>
+                <div className="contact-text">
+                    <h4>{contact.name}</h4>
+                    <p>{contact.message}</p>
+                </div>
+                <div className="contact-noti">
+                    <span>{contact.date}</span>
+                    <div className="noti">2</div>
+                </div>
+        </button>
     )
 }
 
