921c511f850c2993630681896ce313896742d9cd
Sujal Lama <lsujal41@gmail.com>
2021-04-27 18:00:50 +0545

    my-library-articles-responsive

--- src/Components/SurveyCard/SurveyCard.jsx
+++ src/Components/SurveyCard/SurveyCard.jsx
@@ -1,50 +1,11 @@
 import React from 'react'
 import './survey-card.css'
 
-const surveyData = [
-    {
-      _id:1,
-      title: " COVID 19 problems &amp; business",
-      subtitle: " From “Nature Lovers”",
-      subtitle2:" 55 people already answered",
-      answer:"Answer",
-      dismiss:"DIsmiss",
-      cardImage:"/img/card-image.svg"
-    },
-  
-    {
-        _id:2,
-        title: " Do you have horses on your own farm? How do you like it?",
-        subtitle: " From “Nature Lovers”",
-        subtitle2:" 56 people already answered",
-        answer:"Answer",
-        dismiss:"DIsmiss",
-        cardImage:"/img/survey-card-2.svg"
-      },
-      {
-        _id:3,
-        title: " Do you have horses on your own farm? How do you like it?",
-        subtitle: " From “Nature Lovers”",
-        subtitle2:" 60 people already answered",
-        answer:"Answer",
-        dismiss:"DIsmiss",
-        cardImage:"/img/survey-card-3.svg"
-      },
-      {
-        _id:4,
-        title: " What Iphone can bring to you?",
-        subtitle: " From “Nature Lovers”",
-        subtitle2:" 65 people already answered",
-        answer:"Answer",
-        dismiss:"Dismiss",
-        cardImage:"/img/survey-card-4.svg"
-      },
-  ];
 
-const SurveyCard = () => {
+const SurveyCard = ({data}) => {
     return (
         <>
-        {surveyData.map((data)=>{
+        {data.map((data)=>{
             return(
                 <>
                 <div className="survey-card border-1px-onyx">
--- src/Screens/Dashboard/MySurvey.jsx
+++ src/Screens/Dashboard/MySurvey.jsx
@@ -4,6 +4,46 @@ import SurveyCard from "../../Components/SurveyCard/SurveyCard";
 import DashboardLayout from "../../Layout/DashboardLayout/DashboardLayout";
 import "./my-survey.css";
 
+const surveyData = [
+    {
+      _id:1,
+      title: " COVID 19 problems &amp; business",
+      subtitle: " From “Nature Lovers”",
+      subtitle2:" 55 people already answered",
+      answer:"Answer",
+      dismiss:"DIsmiss",
+      cardImage:"/img/card-image.svg"
+    },
+  
+    {
+        _id:2,
+        title: " Do you have horses on your own farm? How do you like it?",
+        subtitle: " From “Nature Lovers”",
+        subtitle2:" 56 people already answered",
+        answer:"Answer",
+        dismiss:"DIsmiss",
+        cardImage:"/img/survey-card-2.svg"
+      },
+      {
+        _id:3,
+        title: " Do you have horses on your own farm? How do you like it?",
+        subtitle: " From “Nature Lovers”",
+        subtitle2:" 60 people already answered",
+        answer:"Answer",
+        dismiss:"DIsmiss",
+        cardImage:"/img/survey-card-3.svg"
+      },
+      {
+        _id:4,
+        title: " What Iphone can bring to you?",
+        subtitle: " From “Nature Lovers”",
+        subtitle2:" 65 people already answered",
+        answer:"Answer",
+        dismiss:"Dismiss",
+        cardImage:"/img/survey-card-4.svg"
+      },
+  ];
+
 function App() {
   return (
     <DashboardLayout title="My Survey">
@@ -19,7 +59,7 @@ function MySurvey() {
       <div className="survey-container">
         <BackButton location={"/dashboard"} />
         <div className="survey-card-container">
-          <SurveyCard />
+          <SurveyCard data={surveyData}/>
         </div>
       </div>
     </div>
