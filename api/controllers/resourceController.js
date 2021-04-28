4717fe3ff78ca23cc249bf3a27601c59134acc3f
Laxman Maharjan <email@mlaxman.com.np>
2021-04-28 13:14:35 +0545

    utils

--- api/controllers/resourceController.js
+++ api/controllers/resourceController.js
@@ -1,35 +1,23 @@
 const Resource = require('../models/resourceModel.js')
 const Sequelize = require('sequelize')
+const queryUtils = require('../utils/query.js')
 const Op = Sequelize.Op
 
 // @desc    Fetch all resources
 // @route   GET /api/resources
 // @access  Public
-const paginate = ({ page, pageSize }) => {
-  const offset = page * pageSize
-  const limit = offset + pageSize
-
-  return {
-    offset,
-    limit
-  }
-}
-
 const getResources = (req, res) => {
   const pageSize = 10
   const page = Number(req.query.pageNumber) || 0
   const order = req.query.order || 'DESC'
-  const ordervalue = order && [['title',  order]] 
-  Resource.findAll({ offset: page, limit: pageSize,ordervalue  })
+  const ordervalue = order && [['title', order]]
+  Resource.findAll({ offset: page, limit: pageSize, ordervalue })
     .then(resources => {
-      paginate({ page, pageSize })
+      queryUtils.paginate({ page, pageSize })
       res.json({ resources, page, pageSize }).status(200)
     })
-    
     .catch((err) => res.json({ err }).status(400))
 }
-
-
 
 // @desc    Add individual resource
 // @route   POST /api/resources/add
@@ -39,10 +27,10 @@ const addResource = (req, res) => {
     title, author, year, description, tag, language, publisher, linkToLicense, subject, level, mediaType, resourceFor, openWith, resourceType, isDownloadable, attachments
   } = req.body
 
-  if (req.file) {
+  /* if (req.file) {
     categoryObj.categoryImage =
-      process.env.API + "/public/" + req.file.filename;
-  }
+      process.env.API + '/public/' + req.file.filename;
+  } */
   Resource.create({
     title, author, year, description, tag, language, publisher, linkToLicense, subject, level, mediaType, resourceFor, openWith, resourceType, isDownloadable, attachments
   })
