fd5fb1a9ffff0342f2af19c2bf4e07eeb97c2ee4
Dixit Sigdel <dixitsigdel8@gmail.com>
2021-04-28 21:25:08 +0545

    linter

--- api/migrations/20210428142824-Collection.js
+++ api/migrations/20210428142824-Collection.js
@@ -1,44 +1,30 @@
-'use strict';
-//todo relation mapping between collection and resources 
+//todo relation mapping between collection and resources
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    queryInterface.createTable('collections',{
-      id:{
-        type:Sequelize.INTEGER,
-        primaryKey:true,
-        autoIncrement:true
+    queryInterface.createTable("collections",
+     {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
       },
-      name:{
-        type:Sequelize.TEXT
+      name: {
+        type: Sequelize.TEXT,
       },
-      docType:{
-        type:Sequelize.TEXT
+      docType: {
+        type: Sequelize.TEXT,
       },
-      resourceType:{
-        type:TEXT
+      resourceType: {
+        type: TEXT,
       },
-      linkId:{
-        type:Sequelize.INTEGER
+      linkId: {
+        type: Sequelize.INTEGER,
       }
-      
     })
-    /**
-     * Add altering commands here.
-     *id, name, docType. db, linkId
-
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-    
+   
   },
 
   down: async (queryInterface, Sequelize) => {
-    queryInterface.dropTable('courses',{})
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    queryInterface.dropTable("courses", {})
   }
 };
