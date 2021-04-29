3c14fa9f799b24d13d1e6363ffecf2f4a9a1edc5
Dixit Sigdel <dixitsigdel8@gmail.com>
2021-04-29 16:15:15 +0545

    linter

--- api/models/collectionModel.js
+++ api/models/collectionModel.js
@@ -1,20 +1,27 @@
 const Sequelize=require('sequelize')
 const db=require('../config/database.js')
 
-const Collection=db.define("collections",
+const Collection = db.define("collections",
 {
-    name:{
-        type:Sequelize.TEXT,
+    id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+    name: {
+        type: Sequelize.TEXT
     },
-    docType:{
-        type:Sequelize.TEXT,
+    docType: {
+        type: Sequelize.TEXT
     },
-    resourceType:{
-        type:Sequelize.TEXT,
+    resourceType: {
+        type: Sequelize.TEXT
     },
-    linkId:{
-        type:Sequelize.INTEGER
+    linkId: {
+        type: Sequelize.INTEGER
     }
-},{timestamps:true})
+},
+ {timestamps: true}
+)
 
-module.exports=Collection;
+module.exports = Collection;
