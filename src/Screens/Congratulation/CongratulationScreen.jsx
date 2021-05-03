f0df0074ab8b8b73782af7700ba70e9b94f88f00
sujal <71555740+SujalLama@users.noreply.github.com>
2021-05-03 21:36:56 +0545

    sign up detail css refactor (fixes #130) (#132)

--- src/Screens/Congratulation/CongratulationScreen.jsx
+++ src/Screens/Congratulation/CongratulationScreen.jsx
@@ -186,17 +186,6 @@ const submitForm = (e) => {
 
 export default CongratulationScreen;
 
-function Frame4(props) {
-  const { children } = props
-
-  return (
-    <div className="frame-4 border-0-5px-quarter-spanish-white">
-      <div className="default-i905507538 valign-text-middle ibmplexsans-semi-bold-quarter-spanish-white-16px">
-        {children}
-      </div>
-    </div>
-  )
-}
 const frame4Data = {
     children: "Skip for now"
 }
