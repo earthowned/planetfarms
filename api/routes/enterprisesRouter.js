562f99b0a2b7cd416e575a33c958cdee356b396b
Dixit Sigdel <dixitsigdel8@gmail.com>
2021-05-03 14:50:20 +0545

    enterprises-api

---
+++ api/routes/enterprisesRouter.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router()
+const {multipleUpload}=require('../helpers/filehelpers')
+
+const { getEnterprises, addEnterprises, getEnterprisesById, deleteEnterprises, updateEnterprises, searchEnterprisesTitle } = require('../controllers/enterprisesController')
+
+
+router.route('/').get(getEnterprises)
+router.route('/add').post( addEnterprises)
+router.route('/search').get(searchEnterprisesTitle)
+router
+  .route('/:id')
+  .get(getEnterprisesById)
+  .delete(deleteEnterprises)
+  .put(updateEnterprises)
+
+module.exports = router
