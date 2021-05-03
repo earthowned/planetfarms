6621f4b028059359d6f62e5dfa6df4f4d7241cd2
Dixit Sigdel <dixitsigdel8@gmail.com>
2021-05-03 15:26:18 +0545

    linter

--- api/routes/enterprisesRouter.js
+++ api/routes/enterprisesRouter.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const {multipleUpload}=require('../helpers/filehelpers')
+const { multipleUpload } = require('../helpers/filehelpers')
 
 const { getEnterprises, addEnterprises, getEnterprisesById, deleteEnterprises, updateEnterprises, searchEnterprisesTitle } = require('../controllers/enterprisesController')
 
-
 router.route('/').get(getEnterprises)
-router.route('/add').post( addEnterprises)
+router.route('/add').post(multipleUpload, addEnterprises)
 router.route('/search').get(searchEnterprisesTitle)
 router
   .route('/:id')
