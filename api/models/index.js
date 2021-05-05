8320fdf574aa8a7eb4a58cd4b6d965e973e2ab06
Laxman Maharjan <email@mlaxman.com.np>
2021-05-05 12:33:50 +0545

    use env for migration and seed

--- api/models/index.js
+++ api/models/index.js
@@ -8,15 +8,9 @@ const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 let sequelize;
-console.log(config);
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+sequelize = new Sequelize(config.database, config.username, config.password, config);
 
-fs
-  .readdirSync(__dirname)
+fs.readdirSync(__dirname)
   .filter(file => {
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
