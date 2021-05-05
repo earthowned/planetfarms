bae658ec673404efe22cbc2587a723be064fe350
Laxman Maharjan <email@mlaxman.com.np>
2021-05-05 08:15:54 +0545

    update db env and merge conflit

--- api/routes/resourceRouter.js
+++ api/routes/resourceRouter.js
@@ -1,24 +1,17 @@
 const express = require('express')
 const router = express.Router()
-<<<<<<< HEAD
 const fs = require('fs')
 const multer = require('multer')
 const shortid = require('shortid')
 const path = require('path')
 const { protect } = require('../middleware/authMiddleware')
-=======
-const {multipleUpload}=require('../helpers/filehelpers')
->>>>>>> ceda9c89 (file attachments)
+const { multipleUpload } = require('../helpers/filehelpers')
 
 const { getResources, addResource, getResourcesById, deleteResources, updateResources, searchResourcesTitle } = require('../controllers/resourceController.js')
 
-
 router.route('/').get(getResources)
-<<<<<<< HEAD
-router.route('/add').post(protect, upload.single('avatar'), addResource)
-=======
+// router.route('/add').post(protect, upload.single('avatar'), addResource)
 router.route('/add').post(multipleUpload, addResource)
->>>>>>> ceda9c89 (file attachments)
 router.route('/search').get(searchResourcesTitle)
 router
   .route('/:id')
