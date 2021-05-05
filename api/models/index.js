a04604bdbd453eb7eb4f53f77164c64de1824471
Laxman Maharjan <email@mlaxman.com.np>
2021-05-05 13:10:52 +0545

    linter

--- api/models/index.js
+++ api/models/index.js
@@ -3,7 +3,7 @@ const path = require('path')
 const Sequelize = require('sequelize')
 const basename = path.basename(__filename)
 const env = process.env.NODE_ENV || 'development'
-const config = require(__dirname + '/../config/config.js')[env]
+const config = require(path.join(__dirname, '/../config/config.js'))[env]
 const db = {}
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config)
@@ -14,12 +14,12 @@ fs.readdirSync(__dirname)
   })
   .forEach(file => {
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes)
-    db[model.name] = model;
+    db[model.name] = model
   })
 
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
-    db[modelName].associate(db);
+    db[modelName].associate(db)
   }
 })
 
