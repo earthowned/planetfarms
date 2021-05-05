e7a1c1199a5be4b1f4fc52b840f9ea764b6f1587
Laxman Maharjan <email@mlaxman.com.np>
2021-05-05 12:45:38 +0545

    linter

--- api/config/config.js
+++ api/config/config.js
@@ -1,7 +1,7 @@
-require('dotenv').config();
+require('dotenv').config()
 
 module.exports = {
-  'development': {
+  development: {
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
--- api/models/index.js
+++ api/models/index.js
@@ -1,14 +1,12 @@
+const fs = require('fs')
+const path = require('path')
+const Sequelize = require('sequelize')
+const basename = path.basename(__filename)
+const env = process.env.NODE_ENV || 'development'
+const config = require(__dirname + '/../config/config.js')[env]
+const db = {}
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js')[env];
-const db = {};
-
-let sequelize;
-sequelize = new Sequelize(config.database, config.username, config.password, config);
+sequelize = new Sequelize(config.database, config.username, config.password, config)
 
 fs.readdirSync(__dirname)
   .filter(file => {
@@ -17,15 +15,15 @@ fs.readdirSync(__dirname)
   .forEach(file => {
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
-  });
+  })
 
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
-});
+})
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+db.sequelize = sequelize
+db.Sequelize = Sequelize
 
-module.exports = db;
+module.exports = db
