493a340afeb9782a94342496a9367fa5020f1a4c
Laxman Maharjan <email@mlaxman.com.np>
2021-05-05 10:08:44 +0545

    linter

--- api/config/database.js
+++ api/config/database.js
@@ -1,6 +1,6 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require('sequelize')
 
 module.exports = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USERNAME, process.env.DATABASE_PASSWORD, {
-  host: 'process.env.DATABASE_HOST',
-  dialect:  'postgres',
+  host: process.env.DATABASE_HOST,
+  dialect: 'postgres'
 });
