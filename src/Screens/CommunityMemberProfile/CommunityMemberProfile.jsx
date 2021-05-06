edf088060c831e161d672ab76c2118c9acc4f5a1
sujal <71555740+SujalLama@users.noreply.github.com>
2021-05-06 12:57:20 +0545

    community member refactor (fixes #142) (#149)
    Co-authored-by: Laxman Maharjan <email@mlaxman.com.np>
    Co-authored-by: dogi <dogi@users.noreply.github.com>

--- src/Screens/CommunityMemberProfile/CommunityMemberProfile.jsx
+++ src/Screens/CommunityMemberProfile/CommunityMemberProfile.jsx
@@ -1,60 +1,41 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import Backbtn from "../../Components/BackButton/BackButton";
-import DashboardLayout from "../../Layout/DashboardLayout/DashboardLayout";
+import React from 'react'
+import DashboardLayout from '../../Layout/DashboardLayout/DashboardLayout'
 // import DataField from '../../Components/ProfileFormCard/ProfileFormCard'
 import {
   PersonalInformation,
   ContactInformation,
-  AdditionalInformation,
-} from "../../Components/ProfileFormCard/ProfileFormCard";
-import "./community-member-profile.css";
+  AdditionalInformation
+} from '../../Components/ProfileFormCard/ProfileFormCard'
+import './community-member-profile.css'
+import BackButton from '../../Components/BackButton/BackButton'
+import EditInformation from '../../Components/EditInformation/EditInformation'
 
-function App() {
+function App () {
   return (
-    <DashboardLayout title="Mikhail Ugryumov">
+    <DashboardLayout title='Mikhail Ugryumov'>
       <CommunityMembersProfile />
     </DashboardLayout>
-  );
+  )
 }
 
-export default App;
+export default App
 
-const ProfileImage = () => {
+function CommunityMembersProfile () {
   return (
-    <div className="flex-col-4">
-      <div
-        className="profile-image"
-        style={{ backgroundImage: `url("/img/profile-image.svg") ` }}
-      ></div>
-      <div className="button">
-        <div className="msg-btn border-0-5px-quarter-spanish-white">
-          <div className="message-btn-text valign-text-middle ibmplexsans-semi-bold-quarter-spanish-white-16px">
-            Write message
-          </div>
+    <div className='x10-4-0-my-personals'>
+      <div className='flex-col-2'>
+        <div className='frame-2923'>
+          <BackButton location='/dashboard' />
         </div>
-        <div className="default-btn-btn">
-          <div className="follow ibmplexsans-semi-bold-shark-16px"> Follow</div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-function CommunityMembersProfile() {
-  return (
-    <div className="x08-1-0-community-members">
-      <div className="flex-col-6">
-        <Backbtn location={"/community-members"} />
-        <div className="profie-details border-1px-onyx">
-          <div className="information-container">
+        <div className='profile border-1px-onyx'>
+          <div className='profile-info'>
             <PersonalInformation />
             <ContactInformation />
             <AdditionalInformation />
           </div>
-          {ProfileImage()}
+          <EditInformation image='/img/profile-image.svg' follow='follow' />
         </div>
       </div>
     </div>
-  );
+  )
 }
--- src/Screens/Dashboard/MyProfile.jsx
+++ src/Screens/Dashboard/MyProfile.jsx
@@ -1,42 +1,34 @@
-import React from "react";
-import "./my-profile.css";
-import DashboardLayout from "../../Layout/DashboardLayout/DashboardLayout";
-import BackButton from "../../Components/BackButton/BackButton";
+import React from 'react'
+import './my-profile.css'
+import DashboardLayout from '../../Layout/DashboardLayout/DashboardLayout'
+import BackButton from '../../Components/BackButton/BackButton'
 import {
   AdditionalInformation,
   ContactInformation,
-  PersonalInformation,
-} from "../../Components/ProfileFormCard/ProfileFormCard";
+  PersonalInformation
+} from '../../Components/ProfileFormCard/ProfileFormCard'
+import EditInformation from '../../Components/EditInformation/EditInformation'
 
-function MyProfile() {
+function MyProfile () {
   return (
-    <DashboardLayout title="My Profile">
-      <div className="x10-4-0-my-personals">
-        <div className="flex-col-2">
-          <div className="frame-2923">
-            <BackButton location="/dashboard"></BackButton>
+    <DashboardLayout title='My Profile'>
+      <div className='x10-4-0-my-personals'>
+        <div className='flex-col-2'>
+          <div className='frame-2923'>
+            <BackButton location='/dashboard' />
           </div>
-          <div className="profile border-1px-onyx">
-            <div className="profile-info">
+          <div className='profile border-1px-onyx'>
+            <div className='profile-info'>
               <PersonalInformation />
               <ContactInformation />
               <AdditionalInformation />
             </div>
-            <EditInformation />
+            <EditInformation image='/img/DashboardProfilePic.png' />
           </div>
         </div>
       </div>
     </DashboardLayout>
-  );
+  )
 }
 
-export default MyProfile;
-
-function EditInformation() {
-  return (
-   <div className="edit-information-container">
-     <img src="/img/DashboardProfilePic.png" />
-     <button className="edit-btn">Edit Information</button>
-   </div>
-  );
-}
+export default MyProfile
