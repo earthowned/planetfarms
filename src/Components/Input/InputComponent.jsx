94ac4c08ed1592acc5452a0bdf7c9f552c72080c
sujal <71555740+SujalLama@users.noreply.github.com>
2021-05-10 21:46:52 +0545

    all css refactor (fixes: #157 ) (#159)
    Co-authored-by: Laxman Maharjan <email@mlaxman.com.np>

--- src/Components/Input/InputComponent.jsx
+++ src/Components/Input/InputComponent.jsx
@@ -3,19 +3,17 @@ import "./input-component.css";
 
 const InputComponent = ({text, error, image, changeHandler, name, autoFocus}) => {
    const userInput = useRef();
-
-   
     return (
         <>
         <div className="input-container">
           <div className={`default-input ${error ? "user-error" : "border-1px-onyx"}`}>
-                
-                <div className="person-outline">
-                {image && <img className="user-icon" src={image}  alt="person-outline" />}
-                 
-                </div>
+                {image && (<div className="person-outline">
+                  <img className="user-icon" src={image}  alt="person-outline" />
+                  </div>)
+                  }
                 {/* <div className="frame-9"> */}
-                {text ? <div className="text ibmplexsans-regular-normal-monsoon-14px">{name && name}</div> : <div>&nbsp;</div>}
+                <div className="input-content">
+                {text ? <div className="overhead-text">{name && name}</div> : <div>&nbsp;</div>}
                 <input ref={userInput}
                   type={name === "Password" ? 'password' : name==="birthday" ? 'date' : name==="email" ? "email" : 'text'}
                   className="username ibmplexsans-regular-normal-monsoon-16px"
@@ -25,6 +23,7 @@ const InputComponent = ({text, error, image, changeHandler, name, autoFocus}) =>
                   id="userInput"
                   autoFocus = {autoFocus === "autoFocus" && true}
                 />
+                </div>
               </div>
                <p className="error-message">{error ? name && name : " "} </p>
          </div>
