80b85c33b2fd0da24cb54ee008010c2b619d3849
dogi <dogi@users.noreply.github.com>
2021-05-11 13:12:24 -0400

    Update CoursesCard.jsx

--- src/Components/CoursesCard/CoursesCard.jsx
+++ src/Components/CoursesCard/CoursesCard.jsx
@@ -118,17 +118,17 @@ function Star ({ num }) {
   return (
     <>
       {
-            stars.map(item => {
-              return (
-                <svg width='22' height='21' viewBox='0 0 22 21' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                  <path
-                    d='M11 1L14.09 7.26L21 8.27L16 13.14L17.18 20.02L11 16.77L4.82 20.02L6 13.14L1 8.27L7.91 7.26L11 1Z'
-                    fill={num < 6 && num > item ? '#00C688' : '#fff'} stroke={num < 6 && num > item ? '#00C688' : '#fff'} stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'
-                  />
-                </svg>
-              )
-            })
-        }
+        stars.map(item => {
+          return (
+            <svg width='22' height='21' viewBox='0 0 22 21' fill='none' xmlns='http://www.w3.org/2000/svg'>
+              <path
+                d='M11 1L14.09 7.26L21 8.27L16 13.14L17.18 20.02L11 16.77L4.82 20.02L6 13.14L1 8.27L7.91 7.26L11 1Z'
+                fill={num < 6 && num > item ? '#00C688' : '#fff'} stroke={num < 6 && num > item ? '#00C688' : '#fff'} stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'
+              />
+            </svg>
+          )
+        })
+      }
     </>
   )
 }
