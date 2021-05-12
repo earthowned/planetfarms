04b1ab0a04ed3a22be89ca365840e4910870883e
dixitsigdel8 <72726217+dixitsigdel8@users.noreply.github.com>
2021-05-12 22:56:49 +0545

    enterprise create form (fixes #179) (#158)
    Co-authored-by: Laxman Maharjan <email@mlaxman.com.np>

--- src/Components/CommunityGroupCard/CommunityGroupCard.jsx
+++ src/Components/CommunityGroupCard/CommunityGroupCard.jsx
@@ -1,64 +1,69 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import Button from "../Button/Button";
-import Secondarybtn from "../SecondaryBtn/Secondarybtn";
-import "./community-group-card.css";
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import Button from '../Button/Button'
+import Secondarybtn from '../SecondaryBtn/Secondarybtn'
+import './community-group-card.css'
 
-const CommunityGroupCard = ({ location }) => {
-  const [follow, setFollow] = useState(false);
+const CommunityGroupCard = ({ location, data }) => {
+  const [follow, setFollow] = useState(false)
   const followClick = () => {
-    setFollow(!follow);
-  };
+    setFollow(!follow)
+  }
 
   return (
-    <div className="card-1 border-1px-onyx">
-      {/* <Link to={location} style={{ textDecoration: "none" }}> */}
-        <div className="card-container" style={{backgroundImage: "url(/img/farmer.svg)"}}></div>
-      {/* </Link> */}
+    <>
+      {data && data.map(item => {
+        return (
 
-      <div className="community-group-card-inner-content">
-      <div className="card-text-container">
-        <Link to={location} style={{ textDecoration: "none" }}>
-          <div className="farmers ibmplexsans-semi-bold-caribbean-green-14px">
-            FARMERS
-          </div>
-        </Link>
-        <Link to={location} style={{ textDecoration: "none" }}>
-          <div className="think-like-a-farmer ibmplexsans-semi-bold-quarter-spanish-white-24px">
-            Think like a farmer
-          </div>
-        </Link>
-      </div>
+          <div key={item.id} className='card-1 border-1px-onyx'>
+            <div className='card-container' style={{ backgroundImage: 'url(/img/farmer.svg)' }} />
 
-      <div className="follower-container">
-        <div className={`follower-number-container`}>
-          <div className="follower-number-item ibmplexsans-regular-normal-monsoon-16px">
-            2,564 followers
-          </div>
-          <p className="follower-number-item ibmplexsans-regular-normal-monsoon-16px">
-            Last publication: 2 days ago
-          </p>
-        </div>
-        {follow ? (
-          <div className="card-btn-container">
-          <div className="card-secondary-btn-wrapper">
-          <Secondarybtn clickHandler={followClick} name="Unfollow" />
-          </div>
-          </div>
-        ) : (
-          <div className="card-btn-container">
-            <div className="card-secondary-btn-wrapper">
-            <Secondarybtn name="Don't recommend" />
+            <div className='community-group-card-inner-content'>
+              <div className='card-text-container'>
+                <Link to={location} style={{ textDecoration: 'none' }}>
+                  <div className='farmers ibmplexsans-semi-bold-caribbean-green-14px'>
+                    {item.title}
+                  </div>
+                </Link>
+                <Link to={location} style={{ textDecoration: 'none' }}>
+                  <div className='think-like-a-farmer ibmplexsans-semi-bold-quarter-spanish-white-24px'>
+                    {item.description}
+                  </div>
+                </Link>
+              </div>
+
+              <div className='follower-container'>
+                <div className='follower-number-container'>
+                  <div className='follower-number-item ibmplexsans-regular-normal-monsoon-16px'>
+                    2,564 followers
+                  </div>
+                  <p className='follower-number-item ibmplexsans-regular-normal-monsoon-16px'>
+                    Last publication: 2 days ago
+                  </p>
+                </div>
+                {follow ? (
+                  <div className='card-btn-container'>
+                    <div className='card-secondary-btn-wrapper'>
+                      <Secondarybtn clickHandler={followClick} name='Unfollow' />
+                    </div>
+                  </div>
+                ) : (
+                  <div className='card-btn-container'>
+                    <div className='card-secondary-btn-wrapper'>
+                      <Secondarybtn name="Don't recommend" />
+                    </div>
+                    <div className='card-secondary-btn-wrapper'>
+                      <Button name='Follow' clickHandler={followClick} />
+                    </div>
+                  </div>
+                )}
+              </div>
             </div>
-            <div className="card-secondary-btn-wrapper">
-            <Button name="Follow" clickHandler={followClick}></Button>
-            </div>
           </div>
-        )}
-      </div>
-      </div>
-    </div>
-  );
-};
+        )
+      })}
+    </>
+  )
+}
 
-export default CommunityGroupCard;
+export default CommunityGroupCard
