b576b685fc4d05db63dfd8a02c18077baf9ab2c4
Sujal Lama <lsujal41@gmail.com>
2021-05-14 16:15:28 +0545

    user collection for courses

--- src/App.jsx
+++ src/App.jsx
@@ -34,6 +34,7 @@ import Courses from './Screens/Courses/Courses'
 import LessonPage from './Screens/Dashboard/LessonPage/LessonPage'
 import Amplify, { Auth } from 'aws-amplify'
 import LessonTest from './Screens/LessonTest/LessonTest'
+import CourseUsers from './Screens/Courses/CourseUsers/CourseUsers'
 
 function amplifyConfig () {
   Amplify.configure({
@@ -156,8 +157,11 @@ function App () {
         <Route path='/community-group-view-page/:id'>
           <CommunityGroupViewPage />
         </Route>
-        <Route path='/courses'>
+        <Route exact path='/courses'>
           <Courses />
+        </Route>
+         <Route path='/courses/users'>
+          <CourseUsers />
         </Route>
         <Route path='/enterprises'>
           <Enterprises />
