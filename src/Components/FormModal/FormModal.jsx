41a461a0449d9df21c740f83947081e549538d1e
Laxman Maharjan <email@mlaxman.com.np>
2021-05-18 22:43:12 +0545

    test for complexity

--- src/Components/FormModal/FormModal.jsx
+++ src/Components/FormModal/FormModal.jsx
@@ -6,6 +6,64 @@ import { useDispatch } from 'react-redux'
 import { createGroup } from '../../actions/communityGroupActions'
 import { createEnterprise } from '../../actions/enterpriseAction'
 
+const { getRootProps, getInputProps } = useDropzone({
+  accept: 'image/*',
+  onDrop: (acceptedFiles) => {
+    setFiles(
+      acceptedFiles.map((file) =>
+        Object.assign(file, {
+          preview: URL.createObjectURL(file)
+        })
+      )
+    )
+  }
+})
+
+const groupTitleChange = (e) => {
+  setGroupTitle(e.target.value)
+  setGroupTitleError(false)
+}
+
+const groupDescriptionChange = (e) => {
+  setGroupDescription(e.target.value)
+  setGroupDescriptionError(false)
+}
+
+const handleAddGroup = async (e) => {
+  e.preventDefault()
+  if (!groupTitle) setGroupTitleError(true)
+  if (!groupDescription) setGroupDescriptionError(true)
+  if (groupTitle && groupDescription) {
+    dispatch(createGroup({ title: groupTitle, description: groupDescription }))
+    setActive(false)
+  }
+}
+
+const enterpriseTitleChange = (e) => {
+  setEnterpriseTitle(e.target.value)
+  setEnterpriseTitleError(false)
+}
+
+const enterpriseDescriptionChange = (e) => {
+  setEnterpriseDescription(e.target.value)
+  setEnterpriseDescriptionError(false)
+}
+
+const handleAddEnterprise = async (e) => {
+  e.preventDefault()
+  if (!enterpriseTitle) setEnterpriseTitleError(true)
+  if (!enterpriseDescription) setEnterpriseDescriptionError(true)
+  console.log(enterpriseTitle, enterpriseDescription)
+  if (enterpriseTitle && enterpriseDescription) {
+    dispatch(createEnterprise({ title: enterpriseTitle, description: enterpriseDescription }))
+    setActive(false)
+  }
+}
+
+const fileChange = (e) => {
+  setFiles(e.target.files[0])
+}
+
 const GroupFromModal = ({ setActive, openAddCollection }) => {
   const [files, setFiles] = useState([])
   const [roleActive, setRoleActive] = useState(false)
@@ -22,63 +80,8 @@ const GroupFromModal = ({ setActive, openAddCollection }) => {
   const dispatch = useDispatch()
 
   const { pathname } = useLocation()
-  const { getRootProps, getInputProps } = useDropzone({
-    accept: 'image/*',
-    onDrop: (acceptedFiles) => {
-      setFiles(
-        acceptedFiles.map((file) =>
-          Object.assign(file, {
-            preview: URL.createObjectURL(file)
-          })
-        )
-      )
-    }
-  })
 
-  const groupTitleChange = (e) => {
-    setGroupTitle(e.target.value)
-    setGroupTitleError(false)
-  }
-
-  const groupDescriptionChange = (e) => {
-    setGroupDescription(e.target.value)
-    setGroupDescriptionError(false)
-  }
-
-  const handleAddGroup = async (e) => {
-    e.preventDefault()
-    if (!groupTitle) setGroupTitleError(true)
-    if (!groupDescription) setGroupDescriptionError(true)
-    if (groupTitle && groupDescription) {
-      dispatch(createGroup({ title: groupTitle, description: groupDescription }))
-      setActive(false)
-    }
-  }
-
-  const enterpriseTitleChange = (e) => {
-    setEnterpriseTitle(e.target.value)
-    setEnterpriseTitleError(false)
-  }
-
-  const enterpriseDescriptionChange = (e) => {
-    setEnterpriseDescription(e.target.value)
-    setEnterpriseDescriptionError(false)
-  }
-
-  const handleAddEnterprise = async (e) => {
-    e.preventDefault()
-    if (!enterpriseTitle) setEnterpriseTitleError(true)
-    if (!enterpriseDescription) setEnterpriseDescriptionError(true)
-    console.log(enterpriseTitle, enterpriseDescription)
-    if (enterpriseTitle && enterpriseDescription) {
-      dispatch(createEnterprise({ title: enterpriseTitle, description: enterpriseDescription }))
-      setActive(false)
-    }
-  }
-
-  const fileChange = (e) => {
-    setFiles(e.target.files[0])
-  }
+  
 
   return (
     <>
