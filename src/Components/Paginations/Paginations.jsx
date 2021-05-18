932c79ea3fcaf7ca9d1a17727f68a1b08451d920
Sujal Lama <lsujal41@gmail.com>
2021-05-18 11:40:56 +0545

    pagination-design

--- src/Components/Paginations/Paginations.jsx
+++ src/Components/Paginations/Paginations.jsx
@@ -1,22 +1,31 @@
 import './pagination.css'
 
+const pages = [1, 2, 3, 4];
 const Pagination = ({ pageNumber, resourceList, setPageNumber }) => {
   function handlePagination (num, multiplier) {
     setPageNumber((pageNumber += num * multiplier))
   }
-
   return (
     <div className='pagination-wrapper'>
       <div className='pagination'>
         <button type='button' disabled={pageNumber == 1} onClick={() => handlePagination(1, -1)}>
-          ❮{pageNumber - 1}
+          Prev
         </button>
+        {
+          //Pages can be replaced with resourceList.totalPages
+          pages.map(num => <PageNumber num={num} pageNumber={pageNumber} setPageNumber={setPageNumber} />)
+        }
         <button type='button' disabled={pageNumber >= resourceList.totalPages} onClick={() => handlePagination(1, +1)}>
-          ❯{pageNumber + 1}
+          Next
         </button>
       </div>
     </div>
   )
 }
 
+function PageNumber ({num, pageNumber, setPageNumber}) {
+  return (
+    <span onClick={() => setPageNumber(num)} className={num === pageNumber && "active"}>{num}</span>
+  )
+}
 export default Pagination
