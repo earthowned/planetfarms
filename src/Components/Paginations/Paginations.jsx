7e1630fbaa6c07713d52dc437071cfae5127e22a
Sujal Lama <lsujal41@gmail.com>
2021-05-18 18:37:25 +0545

    pagination design with button

--- src/Components/Paginations/Paginations.jsx
+++ src/Components/Paginations/Paginations.jsx
@@ -1,10 +1,19 @@
+import {useEffect, useState} from 'react';
 import './pagination.css'
 
-const pages = [1, 2, 3, 4];
-const Pagination = ({ pageNumber, resourceList, setPageNumber }) => {
-  function handlePagination (num, multiplier) {
+const pages = [1, 2, 3, 4, 5, 6];
+const Pagination = ({ pageNumber = 1, resourceList, setPageNumber }) => {
+ const [newPages, setNewPages] = useState([]);  
+  useEffect(() => {
+    if(pageNumber < (pages.length - 2)) {
+   setNewPages(pages.slice((pageNumber - 1), pageNumber + 3));
+    }
+  }, [pageNumber, setPageNumber, newPages])
+
+   function handlePagination (num, multiplier) {
     setPageNumber((pageNumber += num * multiplier))
   }
+  
   return (
     <div className='pagination-wrapper'>
       <div className='pagination'>
@@ -13,7 +22,7 @@ const Pagination = ({ pageNumber, resourceList, setPageNumber }) => {
         </button>
         {
           //Pages can be replaced with resourceList.totalPages
-          pages.map(num => <PageNumber num={num} pageNumber={pageNumber} setPageNumber={setPageNumber} />)
+          newPages.map(num => <PageNumber num={num} pageNumber={pageNumber} setPageNumber={setPageNumber} />)
         }
         <button type='button' disabled={pageNumber >= resourceList.totalPages} onClick={() => handlePagination(1, +1)}>
           Next
@@ -24,6 +33,7 @@ const Pagination = ({ pageNumber, resourceList, setPageNumber }) => {
 }
 
 function PageNumber ({num, pageNumber, setPageNumber}) {
+  console.log("page number", num + 1);
   return (
     <span onClick={() => setPageNumber(num)} className={num === pageNumber && "active"}>{num}</span>
   )
