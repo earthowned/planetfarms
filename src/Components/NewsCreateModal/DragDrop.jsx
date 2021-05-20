e1824b538118db439523080780e0cfea7a8298cc
dogi <dogi@users.noreply.github.com>
2021-05-20 11:40:48 -0400

    Update DragDrop.jsx

--- src/Components/NewsCreateModal/DragDrop.jsx
+++ src/Components/NewsCreateModal/DragDrop.jsx
@@ -7,12 +7,13 @@ const DragDrop = ({ getInputProps, getRootProps, files }) => {
       {files != null
         ? (
           <img className='avatar' src={files[0].preview} alt='files[0].preview' />
-          )
+        )
         : (
           <h6 className='text-4 valign-text-middle ibmplexsans-semi-bold-quarter-spanish-white-16px'>
             Drag & Drop files in this area or Click Here to attach video cover
           </h6>
-          )}
+        )
+      }
     </div>
   )
 }
