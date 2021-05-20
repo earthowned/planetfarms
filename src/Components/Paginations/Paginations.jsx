5482ba7e510b896efd1b81fdc9f8a806f69e5f6f
Sujal Lama <lsujal41@gmail.com>
2021-05-20 15:32:41 +0545

    pagination in different page sizes

--- src/Components/Paginations/Paginations.jsx
+++ src/Components/Paginations/Paginations.jsx
@@ -8,37 +8,41 @@ const Pagination = ({ pageNumber = 1, resourceList, setPageNumber }) => {
   let addedPages = [...Array(resourceList.totalPages).keys()];
   let newAddedPages = addedPages.map(num => num + 1);
   setNewPages(newAddedPages);
-  
-  if(pageNumber === 1) {
-     return setNewUpdatedPages(newPages.slice(pageNumber, pageNumber + 2));
-  }
-  if(pageNumber === 2) {
-    return setNewUpdatedPages(newPages.slice(pageNumber -1, pageNumber + 2));
-  }
+  setNewUpdatedPages(newAddedPages);
+  if(newPages.length > 4) {
+          if(pageNumber === 1) {
+          return setNewUpdatedPages(newPages.slice(pageNumber, pageNumber + 2));
+        }
+        if(pageNumber === 2) {
+          return setNewUpdatedPages(newPages.slice(pageNumber -1, pageNumber + 2));
+        }
 
-  if(pageNumber === 3) {
-    return setNewUpdatedPages(newPages.slice(pageNumber -2, pageNumber + 2));
-  }
+        if(pageNumber === 3) {
+          return setNewUpdatedPages(newPages.slice(pageNumber -2, pageNumber + 2));
+        }
 
-  //for third last number
-  if(pageNumber === (resourceList.totalPages - 2)) {
-     return setNewUpdatedPages(newPages.slice((pageNumber - 3), pageNumber + 1));
-  }
+        //for third last number
+        if(pageNumber === (resourceList.totalPages - 2)) {
+          return setNewUpdatedPages(newPages.slice((pageNumber - 3), pageNumber + 1));
+        }
 
-  //for second last number
-  if(pageNumber === (resourceList.totalPages - 1)) {
-     return setNewUpdatedPages(newPages.slice((pageNumber - 3), pageNumber));
-  }
+        //for second last number
+        if(pageNumber === (resourceList.totalPages - 1)) {
+          return setNewUpdatedPages(newPages.slice((pageNumber - 3), pageNumber));
+        }
 
-  // for last number
-  if(pageNumber === resourceList.totalPages) {
-    return setNewUpdatedPages(newPages.slice((pageNumber - 4), pageNumber - 1));
-  }
+        // for last number
+        if(pageNumber === resourceList.totalPages) {
+          return setNewUpdatedPages(newPages.slice((pageNumber - 4), pageNumber - 1));
+        }
 
-  // for other number
-  if(pageNumber < (resourceList.totalPages - 1)) {
-   setNewUpdatedPages(newPages.slice((pageNumber - 3), pageNumber + 2));
+        // for other number
+        if(pageNumber < (resourceList.totalPages - 1)) {
+        setNewUpdatedPages(newPages.slice((pageNumber - 3), pageNumber + 2));
     }
+    
+  }
+  
   }, [resourceList, handlePagination])
  
    function handlePagination (num, multiplier) {
@@ -51,12 +55,12 @@ const Pagination = ({ pageNumber = 1, resourceList, setPageNumber }) => {
         <button type='button' disabled={pageNumber == 1} onClick={() => handlePagination(1, -1)}>
           Prev
         </button>
-        <span onClick={() => setPageNumber(1)} className={pageNumber === 1 && "active"}>1</span>
+        {newPages.length > 4 && <span onClick={() => setPageNumber(1)} className={pageNumber === 1 && "active"}>1</span>}
         {
           //Pages can be replaced with resourceList.totalPages
           newUpdatedPages && newUpdatedPages.map(num => <PageNumber num={num} pageNumber={pageNumber} setPageNumber={setPageNumber} />)
         }
-        <span onClick={() => setPageNumber(resourceList.totalPages)} className={resourceList.totalPages === pageNumber && "active"}>{resourceList.totalPages}</span>
+        {newPages.length > 4 && <span onClick={() => setPageNumber(resourceList.totalPages)} className={resourceList.totalPages === pageNumber && "active"}>{resourceList.totalPages}</span>}
         <button type='button' disabled={pageNumber >= resourceList.totalPages} onClick={() => handlePagination(1, +1)}>
           Next
         </button>
