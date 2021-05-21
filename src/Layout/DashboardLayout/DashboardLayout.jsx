6ff871fffbca684bdc1f894465dbaeba4bd7cafa
Laxman Maharjan <email@mlaxman.com.np>
2021-05-21 12:21:09 +0545

    fix layout

--- src/Layout/DashboardLayout/DashboardLayout.jsx
+++ src/Layout/DashboardLayout/DashboardLayout.jsx
@@ -122,20 +122,20 @@ const DashboardLayout = ({ title, children }) => {
             <h1>{title}</h1>
             <div >{children}</div>
             <div className="space-taker"></div>
-        </div>
-
-        <div className="footer-nav">
-          <div onClick={() => activeUser()} className={`mobile-tab-wrapper ${userActive && "bgactive"} `}>
-            <Link to="/dashboard"><UserMenu activeUser={activeUser} /></Link>
           </div>
-          <div onClick={() => activeMessage()} className={`mobile-tab-wrapper ${messageActive && "bgactive"}`}>
-            <MessageMenu activeMessage={activeMessage}/>
-          </div>
-          <div onClick={() => activeNotification()} className={`mobile-tab-wrapper ${notificationActive && "bgactive"}`}> 
-            <NotificationMenu activeNotification={activeNotification} />
-          </div>
-          <div onClick={(e) => { activeBurger(); SignOutModal(e) } } className={`mobile-tab-wrapper ${burgerActive && "bgactive"}`}>
-            <Hamburger activeBurger={activeBurger} />
+          <div className="footer-nav">
+            <div onClick={() => activeUser()} className={`mobile-tab-wrapper ${userActive && "bgactive"} `}>
+              <Link to="/dashboard"><UserMenu activeUser={activeUser} /></Link>
+            </div>
+            <div onClick={() => activeMessage()} className={`mobile-tab-wrapper ${messageActive && "bgactive"}`}>
+              <MessageMenu activeMessage={activeMessage}/>
+            </div>
+            <div onClick={() => activeNotification()} className={`mobile-tab-wrapper ${notificationActive && "bgactive"}`}> 
+              <NotificationMenu activeNotification={activeNotification} />
+            </div>
+            <div onClick={(e) => { activeBurger(); SignOutModal(e) } } className={`mobile-tab-wrapper ${burgerActive && "bgactive"}`}>
+              <Hamburger activeBurger={activeBurger} />
+            </div>
           </div>
         </>
       )}
