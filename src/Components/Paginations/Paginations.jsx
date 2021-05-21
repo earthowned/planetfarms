3bc82bcdd927f89d2a94ddecb04a387008e78e06
Laxman Maharjan <email@mlaxman.com.np>
2021-05-21 15:06:01 +0545

    change pagination logic

--- src/Components/Paginations/Paginations.jsx
+++ src/Components/Paginations/Paginations.jsx
@@ -2,40 +2,14 @@ import { useEffect, useState } from 'react'
 import './pagination.css'
 
 const Pagination = ({ pageNumber = 1, resourceList, setPageNumber }) => {
-  const [newPages, setNewPages] = useState([])
-  const [newUpdatedPages, setNewUpdatedPages] = useState([])
+  const [pageNum, setPageNum] = useState([])
   useEffect(() => {
     const addedPages = [...Array(resourceList.totalPages).keys()].map(num => num + 1)
-    setNewPages(addedPages)
-    setNewUpdatedPages(addedPages)
-    if (newPages.length > 4) {
-      if (pageNumber === 1) {
-        return setNewUpdatedPages(newPages.slice(pageNumber, pageNumber + 2))
-      }
-      if (pageNumber === 2) {
-        return setNewUpdatedPages(newPages.slice(pageNumber - 1, pageNumber + 2))
-      }
-      if (pageNumber === 3) {
-        return setNewUpdatedPages(newPages.slice(pageNumber - 2, pageNumber + 2))
-      }
-      // for third last number
-      if (pageNumber === (resourceList.totalPages - 2)) {
-        return setNewUpdatedPages(newPages.slice((pageNumber - 3), pageNumber + 1))
-      }
-      // for second last number
-      if (pageNumber === (resourceList.totalPages - 1)) {
-        return setNewUpdatedPages(newPages.slice((pageNumber - 3), pageNumber))
-      }
-      // for last number
-      if (pageNumber === resourceList.totalPages) {
-        return setNewUpdatedPages(newPages.slice((pageNumber - 4), pageNumber - 1))
-      }
-
-      // for other number
-      if (pageNumber < (resourceList.totalPages - 1)) {
-        setNewUpdatedPages(newPages.slice((pageNumber - 3), pageNumber + 2))
-      }
-    }
+    const offset = (3 - pageNumber) > 0 ? (3 - pageNumber) : 0
+    let visiblePages = addedPages.slice(pageNumber - 3 + offset, pageNumber + 2)
+    if (pageNumber > 3) visiblePages = [1, ...visiblePages]
+    if (addedPages.length - pageNumber > 2) visiblePages.push(resourceList.totalPages)
+    setPageNum(visiblePages)
   }, [resourceList, pageNumber])
 
   function handlePagination (num, multiplier) {
@@ -48,12 +22,10 @@ const Pagination = ({ pageNumber = 1, resourceList, setPageNumber }) => {
         <button type='button' disabled={pageNumber === 1} onClick={() => handlePagination(1, -1)}>
           Prev
         </button>
-        {newPages.length > 4 && <span onClick={() => setPageNumber(1)} className={pageNumber === 1 && 'active'}>1</span>}
         {
           // Pages can be replaced with resourceList.totalPages
-          newUpdatedPages && newUpdatedPages.map(num => <PageNumber num={num} pageNumber={pageNumber} setPageNumber={setPageNumber} />)
+          pageNum && pageNum.map(num => <PageNumber num={num} pageNumber={pageNumber} setPageNumber={setPageNumber} />)
         }
-        {newPages.length > 4 && <span onClick={() => setPageNumber(resourceList.totalPages)} className={resourceList.totalPages === pageNumber && 'active'}>{resourceList.totalPages}</span>}
         <button type='button' disabled={pageNumber >= resourceList.totalPages} onClick={() => handlePagination(1, +1)}>
           Next
         </button>
