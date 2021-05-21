43bd1dc96e13534474d1d37c0dcd6bce26b8a5ee
Robin Giri <45058288+robinGiri@users.noreply.github.com>
2021-05-21 12:28:26 +0545

    api: cognito login (fixes #220) (#211)
    Co-authored-by: Laxman Maharjan <email@mlaxman.com.np>

--- src/Components/MessageCard/MessageCard.jsx
+++ src/Components/MessageCard/MessageCard.jsx
@@ -1,25 +1,45 @@
 import React from 'react'
-import { useHistory } from 'react-router';
-import useSizeFinder from '../../utils/SizeFinder';
+import { useHistory } from 'react-router'
+import useSizeFinder from '../../utils/SizeFinder'
 import './message-card.css'
-const MessageCard = ({contact, clickHandler}) => {
-    const history = useHistory();
-    const windowWidth = useSizeFinder();
-    return (
-        <button className="contact-card" key={contact.name} onClick={() => windowWidth > 850 ? clickHandler(true) : history.push(`/messenger/${contact.name}`)}>
-                <div className="img-wrapper">
-                    <img src={contact.img} />
-                </div>
-                <div className="contact-text">
-                    <h4>{contact.name}</h4>
-                    <p>{contact.message}</p>
-                </div>
-                <div className="contact-noti">
-                    <span>{contact.date}</span>
-                    <div className="noti">2</div>
-                </div>
+
+function TextBlock({contact}) {
+  return (
+    <div className='contact-text'>
+      <h4>{contact?.name}</h4>
+      <p>{contact?.message}</p>
+    </div>
+  )
+}
+
+const MessageCard = ({ contact, clickHandler, profileSettings, setModalActive, clickProfileHandler }) => {
+  const history = useHistory()
+  const windowWidth = useSizeFinder()
+
+  return (
+    <>
+      {profileSettings ? (
+        <button className='last-contact-card' onClick={() => clickProfileHandler(contact)}>
+          <TextBlock contact={contact} />
         </button>
-    )
+      ) : (
+        <button
+          className='contact-card'
+          key={contact.name}
+          onClick={() => (windowWidth > 850 ? clickHandler(true) : history.push(`/messenger/${contact.name}`))}
+        >
+          <div className='img-wrapper'>
+            <img src={contact?.img} alt='contact' />
+          </div>
+          <TextBlock contact={contact} />
+          <div className='contact-noti'>
+            <span>{contact?.date}</span>
+            <div className='noti'>2</div>
+          </div>
+        </button>
+      )}
+    </>
+  )
 }
 
 export default MessageCard
