ef74af6c0d9b381c1723350b22e1dc0726f93a5a
Sujal Lama <lsujal41@gmail.com>
2021-05-25 14:12:14 +0545

    scrolling function refactoring

--- src/utils/scrollFunc.jsx
+++ src/utils/scrollFunc.jsx
@@ -4,25 +4,48 @@ import useSizeFinder from './SizeFinder';
 const useScroll = (scrollRef) => {
     const [scrollActive, setScrollActive] = useState(true);
     const [scrollLeftActive, setScrollLeftActive] = useState(false);
+   const [right, setRight] = useState(0);
+   const [left, setLeft] = useState(0);
+   const [width, setWidth] = useState();
+   
     
-    
-    const scrollRight = (scrollParam) => {
-        scrollParam.current.scrollLeft += 500;
+   const scrollRight = () => {
+        scrollRef.current.scrollBy(500, 0);
+        setRight(existingnum => (500 + existingnum ));
+        setLeft(0);
         setScrollLeftActive(true);
     }
+    
 
-    const scrollLeft = (scrollParam) => {
-            scrollParam.current.scrollLeft = null;
-            setScrollLeftActive(false)
+    const scrollLeft = () => {
+            scrollRef.current.scrollBy(-500, 0);
+            setRight(0);
+            setLeft(existingnum => (500 + existingnum ));
+            setScrollActive(true);
     }
 
     const windowWidth = useSizeFinder();
+     useEffect(() => {
+        if((scrollRef.current.scrollWidth - right ) <= 450) {
+            setScrollActive(false);
+        } 
+
+        if((scrollRef.current.scrollWidth - left ) <= 450) {
+            setScrollLeftActive(false);
+        } 
+
+    }, [right, left])
+
     useEffect(() => {
         if(scrollRef.current.offsetWidth >= scrollRef.current.scrollWidth) {
         setScrollActive(false)
-        };
+        } else {
+            setScrollActive(true)
+        }
+        // setWidth(scrollRef.current.scrollWidth);
     }, [windowWidth])
 
+   
     return {scrollLeftActive, scrollActive, scrollRight, scrollLeft}
 }
 
