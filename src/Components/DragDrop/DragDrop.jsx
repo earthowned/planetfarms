f74f000631a9c7fe6a07bada6ca2dcb0aa9686b2
Sujal Lama <lsujal41@gmail.com>
2021-05-26 18:46:00 +0545

    drag and drop js lint fix

--- src/Components/DragDrop/DragDrop.jsx
+++ src/Components/DragDrop/DragDrop.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import { useDropzone } from 'react-dropzone'
 
-
 const DragDrop = ({ onChange }) => {
   const [files, setFiles] = useState([])
   const { getRootProps, getInputProps } = useDropzone({
@@ -22,22 +21,21 @@ const DragDrop = ({ onChange }) => {
   }
   return (
     <div className='drag-drop-container'>
-    <div className="drag-drop" {...getRootProps()} onChange={(e) => fileChange(e)}>
-      <input {...getInputProps()} />
-      {files.length > 0
-        ? (<>
-          <img className='avatar' src={files[0].preview} alt='files[0].preview' />
-          <div className="drag-drop-icon-container"><img src="/img/camera-outline.svg" alt="camera icon"/></div>
-          </>
-        )
-        : (
-          <h6 className='text-4 valign-text-middle ibmplexsans-semi-bold-quarter-spanish-white-16px'>
-            Drag & Drop files in this area or Click Here to attach video cover
-          </h6>
-        )
-      }
-    </div>
-    {files.length > 0 && <img src="/img/close-outline.svg" className="drag-drop-close" onClick={() => setFiles([])} />}
+      <div className='drag-drop' {...getRootProps()} onChange={(e) => fileChange(e)}>
+        <input {...getInputProps()} />
+        {files.length > 0
+          ? (<>
+            <img className='avatar' src={files[0].preview} alt='files[0].preview' />
+            <div className='drag-drop-icon-container'><img src='/img/camera-outline.svg' alt='camera icon' /></div>
+             </>
+            )
+          : (
+            <h6 className='text-4 valign-text-middle ibmplexsans-semi-bold-quarter-spanish-white-16px'>
+              Drag & Drop files in this area or Click Here to attach video cover
+            </h6>
+            )}
+      </div>
+      {files.length > 0 && <img src='/img/close-outline.svg' className='drag-drop-close' onClick={() => setFiles([])} />}
     </div>
   )
 }
