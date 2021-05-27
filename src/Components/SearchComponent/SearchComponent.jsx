ed993cda6e189f87f913ee9dfcccf790bd79d7d6
Sujal Lama <lsujal41@gmail.com>
2021-05-27 15:17:45 +0545

    js fix in messenger main and search component

--- src/Components/SearchComponent/SearchComponent.jsx
+++ src/Components/SearchComponent/SearchComponent.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
-import './search-component.css';
+import './search-component.css'
 
 const SearchComponent = ({ className, search, setSearch, showEditIcon }) => {
-    return (
-        
-        // <div className={className}>
-        <div className="search-btn">
-         <img src="/img/search-outline-icon.svg" alt="Search" /> <input value={search} type="text"  onChange={ (e) => setSearch(e.target.value)}  className="search-1" placeholder="Search..." />
-         {showEditIcon && <img src="/img/edit-icon.svg" alt="edit icon"/>}
-        {/* </div> */}
-        {/* <button className="edit-btn">
+  return (
+
+  // <div className={className}>
+    <div className='search-btn'>
+      <img src='/img/search-outline-icon.svg' alt='Search' /> <input value={search} type='text' onChange={(e) => setSearch(e.target.value)} className='search-1' placeholder='Search...' />
+      {showEditIcon && <img src='/img/edit-icon.svg' alt='edit icon' />}
+      {/* </div> */}
+      {/* <button className="edit-btn">
         <img src="/img/edit-icon.svg"></img>
         </button> */}
-        </div>
-        // </div>
-        
-    )
+    </div>
+  // </div>
+
+  )
 }
 
 export default SearchComponent
