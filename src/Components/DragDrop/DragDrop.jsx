2c7c9a1465a146f2646da22498d67b349e10e694
Laxman Maharjan <email@mlaxman.com.np>
2021-05-28 10:34:56 +0545

    line break

--- src/Components/DragDrop/DragDrop.jsx
+++ src/Components/DragDrop/DragDrop.jsx
@@ -40,7 +40,8 @@ function DragDropComponent ({ getInputProps, getRootProps, fileChange, files, se
               Drag & Drop files in this area or Click Here to attach video cover
             </h6>
             )}
-      </div>{files && <img src='/img/close-outline.svg' className='drag-drop-close' onClick={() => setFiles(null)} />}
+      </div>
+      {files && <img src='/img/close-outline.svg' className='drag-drop-close' onClick={() => setFiles(null)} />}
     </div>
   )
 }
