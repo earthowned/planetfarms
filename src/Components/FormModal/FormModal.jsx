45fad5807bf7b4fd3e7b59bcd3b4ee06065f5233
Laxman Maharjan <email@mlaxman.com.np>
2021-05-28 12:27:12 +0545

    put enterprise-collection back

--- src/Components/FormModal/FormModal.jsx
+++ src/Components/FormModal/FormModal.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './form-modal.css'
+import '../EnterprisesCollection/enterprises-collection.css'
 import { useLocation } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import { createGroup } from '../../actions/communityGroupActions'
