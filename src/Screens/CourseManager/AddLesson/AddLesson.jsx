76f053db4cbed920641913009dc7b51fca47aade
dogi <dogi@users.noreply.github.com>
2021-06-01 01:07:07 -0400

    Update AddLesson.jsx

--- src/Screens/CourseManager/AddLesson/AddLesson.jsx
+++ src/Screens/CourseManager/AddLesson/AddLesson.jsx
@@ -37,7 +37,7 @@ const AddContent = ({ setVideoModal, setImageModal, setTextModal, setTestModal }
         <button className='secondary-btn' onClick={() => setVideoModal(true)}><img src='/img/video-outline.svg' alt='video icon' /> <span>Add video</span></button>
         <button className='secondary-btn' onClick={() => setImageModal(true)}><img src='/img/image-outline.svg' alt='image icon' /> <span>Add image</span></button>
         <button className='secondary-btn' onClick={() => setTextModal(true)}><img src='/img/text-outline.svg' alt='text icon' /> <span>Add text</span></button>
-         <button className='secondary-btn' onClick={() => setTestModal(true)}><img src='/img/test-outline.svg' alt='test icon' /> <span>Add test</span></button>
+        <button className='secondary-btn' onClick={() => setTestModal(true)}><img src='/img/test-outline.svg' alt='test icon' /> <span>Add test</span></button>
       </div>
     </div>
   )
