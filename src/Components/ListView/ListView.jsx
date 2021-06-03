4880c6f90506f1a12b986c9914bb553cba4968c5
Sujal Lama <lsujal41@gmail.com>
2021-06-03 18:23:12 +0545

    list-view jsx fix

--- src/Components/ListView/ListView.jsx
+++ src/Components/ListView/ListView.jsx
@@ -9,28 +9,28 @@ const ListView = ({ data, title, setNewCollection, setModalActive, modalActive }
       <div className='listview-container'>
         <h4>{title}</h4>
         <div>
-        {data && data.map(item => {
-          return (
-            <div key={item.id} className='listview-inner-container'>
-              <div className="listview-col-1">
-                <div className='image-wrapper'>
-                  <img src={process.env.REACT_APP_CDN_BASE_URL + '/file/' + item.filename} alt='item.title' />
+          {data && data.map(item => {
+            return (
+              <div key={item.id} className='listview-inner-container'>
+                <div className='listview-col-1'>
+                  <div className='image-wrapper'>
+                    <img src={process.env.REACT_APP_CDN_BASE_URL + '/file/' + item.filename} alt='item.title' />
+                  </div>
+                  <div className='list-content'>
+                    <h4>{item.title}</h4>
+                    <p>{item.category}</p>
+                  </div>
                 </div>
-                <div className='list-content'>
-                  <h4>{item.title}</h4>
-                  <p>{item.category}</p>
+                <div className='list-btn-wrapper'>
+                  <span>Add to</span>
+                  <button className='secondary-btn-border btn-img-wrapper' onClick={() => setActive(!active)}>
+                    {active ? (<><img src='./img/checkmark-outline.svg' alt='Added' /> <span>Added</span></>) : (<><img src='./img/book.svg' alt='My library' /> <span>My library</span></>)}
+                  </button>
+                  <button className='secondary-btn-border' onClick={() => setModalActive(!modalActive)}>Collections</button>
                 </div>
               </div>
-              <div className='list-btn-wrapper'>
-                <span>Add to</span>
-                <button className='secondary-btn-border btn-img-wrapper' onClick={() => setActive(!active)}>
-                  {active ? (<><img src='./img/checkmark-outline.svg' alt='Added' /> <span>Added</span></>) : (<><img src='./img/book.svg' alt='My library' /> <span>My library</span></>)}
-                </button>
-                <button className='secondary-btn-border' onClick={() => setModalActive(!modalActive)}>Collections</button>
-              </div>
-            </div>
-          )
-        })}
+            )
+          })}
         </div>
       </div>
     </>
