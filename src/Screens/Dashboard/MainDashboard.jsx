6286069010afca96bd7f4a8879e38b11a46afb37
Sujal Lama <lsujal41@gmail.com>
2021-06-10 14:02:48 +0545

    dashboard fix

--- src/Screens/Dashboard/MainDashboard.jsx
+++ src/Screens/Dashboard/MainDashboard.jsx
@@ -45,7 +45,8 @@ function DashboardComponent () {
 
           </div>
           {windowWidth < 721
-            ? <div className='mobile-container-dasboard-my-profile-box'>
+            ? 
+              <div className='mobile-container-dasboard-my-profile-box'>
               <Link to='/mylibrary' className='dasboard-my-profile-box'>
                 <MobileViewHeaderContent title='My library' image='/img/book-outlined.svg' />
               </Link>
