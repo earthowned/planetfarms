42e45b469ce1e80c2fc61626189f38e91ed0e237
sujal <71555740+SujalLama@users.noreply.github.com>
2021-06-11 12:46:03 +0545

    community switching responsive (fixes #263) (#268)

--- src/Screens/CommunitySwitching/CommunitySwitching.jsx
+++ src/Screens/CommunitySwitching/CommunitySwitching.jsx
@@ -1,40 +1,34 @@
-import React from "react";
-import CommunitiesCard from "../../Components/CommunitiesCard/CommunitiesCard";
-import Filter from "../../Components/Filter/Filter";
-import SearchComponent from "../../Components/SearchComponent/SearchComponent";
-import DashboardLayout from "../../Layout/DashboardLayout/DashboardLayout";
-import "./community-switching.css";
+import React from 'react'
+import CommunitiesCard from '../../Components/CommunitiesCard/CommunitiesCard'
+import Filter from '../../Components/Filter/Filter'
+import SearchComponent from '../../Components/SearchComponent/SearchComponent'
+import DashboardLayout from '../../Layout/DashboardLayout/DashboardLayout'
+import './community-switching.css'
 
-function App() {
+function App () {
   return (
-    <DashboardLayout title="All Communities">
+    <DashboardLayout title='All Communities'>
       <AllCommunities />
     </DashboardLayout>
-  );
+  )
 }
 
-export default App;
+export default App
 
-function AllCommunities(props) {
+function AllCommunities (props) {
   return (
-    <div className="x07-1-0-all-communities">
-      <div className="community-container">
-        <div className="flex-row-4">
-          {/* <SearchComponent className={"search border-1px-onyx"} />
-          <div className="library-sub-header-2">
-              <Filter />
-              </div> */}
-               <div className="library-sub-header">
-              <div className="library-sub-header-1">
-              <SearchComponent className={"search border-1px-onyx"} /> 
-              </div>
-              <div className="library-sub-header-2 filter">
-              <Filter />
-              </div>
-           </div>
+    <div className='x07-1-0-all-communities'>
+      <div className='community-switch-container'>
+        <div className='community-switch-sub-header'>
+          <div className='community-switch-sub-header-1'>
+            <SearchComponent className='search border-1px-onyx' />
+          </div>
+          <div className='community-switch-sub-header-2'>
+            <Filter />
+          </div>
         </div>
         <CommunitiesCard />
       </div>
     </div>
-  );
+  )
 }
