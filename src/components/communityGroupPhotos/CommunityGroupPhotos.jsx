e7fa14c280f50766ab820e6529c34dc0cf5c942d
Laxman Maharjan <email@mlaxman.com.np>
2021-06-18 12:55:33 +0545

    conflict

--- src/components/communityGroupPhotos/CommunityGroupPhotos.jsx
+++ src/components/communityGroupPhotos/CommunityGroupPhotos.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:src/components/communityGroupPhotos/CommunityGroupPhotos.jsx
 import React, { useRef } from 'react'
 import useScroll from '../../utils/scrollFunc'
 import Filter from '../filter/Filter'
@@ -27,55 +26,11 @@ const CommunityGroupPhotos = () => {
       </div>
       <div className='scroll-container'>
         <ScrollItems photo={groupPhoto} />
-=======
-import React from 'react'
-
-const CommunityGroupPhotos = () => {
-    return (
-        <div className="group-photo">
-        <div className="group-photo-title">
-          <div className="enterprise-photos-i5 valign-text-middle ibmplexsans-semi-bold-quarter-spanish-white-24px">
-            Group Photos
-          </div>
-        </div>
-        <div className="flex-row-4">
-          <div
-            className="photo"
-            style={{ backgroundImage: `url(${"/img/group-photo-2.svg"})` }}
-          ></div>
-          <div
-            className="frame-296"
-            style={{ backgroundImage: `url(${"/img/group-photo-1.svg"})` }}
-          ></div>
-          <div
-            className="frame-296"
-            style={{ backgroundImage: `url(${"/img/group-photo-3.svg"})` }}
-          ></div>
-          <div
-            className="frame-296"
-            style={{ backgroundImage: `url(${"/img/group-photo-2.svg"})` }}
-          ></div>
-          <div className="overlap-group1-2">
-            <div
-              className="frame-2969"
-              style={{ backgroundImage: `url(${"/img/group-photo-1.svg"})` }}
-            ></div>
-            <div className="arrow">
-              <div className="arrow-forward-outline-1">
-                <div className="overlap-group-6">
-                  <img className="group-arrow-icon" src="img/arrow-icon.svg" />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
->>>>>>> bcf7f8e... rename:src/Components/communityGroupPhotos/CommunityGroupPhotos.jsx
       </div>
     </div>
   )
 }
 
-<<<<<<< HEAD:src/components/communityGroupPhotos/CommunityGroupPhotos.jsx
 function ScrollItems ({ photo }) {
   const scrollRef = useRef()
   const { scrollLeftActive, scrollActive, scrollRight, scrollLeft } = useScroll(scrollRef)
@@ -113,7 +68,5 @@ function Tiles ({ data }) {
     </>
   )
 }
-=======
->>>>>>> bcf7f8e... rename:src/Components/communityGroupPhotos/CommunityGroupPhotos.jsx
 export default CommunityGroupPhotos
 
