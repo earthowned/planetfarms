86736eda7d230a2e14f3a3851b83bdf8abcc06b2
Puskar Adhikari <puskar.ad@gmail.com>
2021-06-22 12:24:41 +0545

    lesson api router for CRUD operation

---
+++ api/src/migrations/20210621170218-create-table-lessons.js
@@ -0,0 +1,30 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    queryInterface.createTable("lessons", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      courseId: {
+        type: Sequelize.STRING,
+      },
+      lessonId: {
+        type: Sequelize.STRING,
+      },
+      steps: {
+        type: Sequelize.JSON,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    queryInterface.dropTable("lessons", {});
+  },
+};
--- api/src/server.js
+++ api/src/server.js
@@ -1,55 +1,57 @@
-require("dotenv").config();
-const path = require("path");
-const express = require("express");
-const app = express();
-const userRoutes = require("./routes/userRouter.js");
-const resourceRoutes = require("./routes/resourceRouter.js");
-const collectionRoutes = require("./routes/collectionRouter");
-const courseRoutes = require("./routes/courseRouter.js");
-const newsRoutes = require("./routes/newsRouter");
-const enterprisesRoutes = require("./routes/enterprisesRouter");
-const communityGroupsRoutes = require("./routes/communityGroupRouter");
-const calendarRoutes = require("./routes/calendarEventsRouter");
-const categoriesRoutes = require("./routes/categoriesRouter");
-const sequelize = require("./config/database.js");
-const cors = require("cors");
-const dotenv = require("dotenv");
+require('dotenv').config()
+const path = require('path')
+const express = require('express')
+const app = express()
+const userRoutes = require('./routes/userRouter.js')
+const resourceRoutes = require('./routes/resourceRouter.js')
+const collectionRoutes = require('./routes/collectionRouter')
+const courseRoutes = require('./routes/courseRouter.js')
+const newsRoutes = require('./routes/newsRouter')
+const enterprisesRoutes = require('./routes/enterprisesRouter')
+const communityGroupsRoutes = require('./routes/communityGroupRouter')
+const calendarRoutes = require('./routes/calendarEventsRouter')
+const categoriesRoutes = require('./routes/categoriesRouter')
+const lessonsRoutes = require('./routes/lessonsRouter')
+const sequelize = require('./config/database.js')
+const cors = require('cors')
+const dotenv = require('dotenv')
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.port || 5000
 
 // middleware
-app.use(express.json());
-app.use(cors());
-dotenv.config();
+app.use(express.json())
+app.use(cors())
+dotenv.config()
 
 // routes
-app.use("/api/users", userRoutes);
-app.use("/api/resources", resourceRoutes);
-app.use("/api/collection", collectionRoutes);
-app.use("/api/enterprises", enterprisesRoutes);
-app.use("/api/groups", communityGroupsRoutes);
-app.use("/api/calendar", calendarRoutes);
-app.use("/api/courses", courseRoutes);
-app.use("/api/news", newsRoutes);
-app.use("/api/categories", categoriesRoutes);
+app.use('/api/users', userRoutes)
+app.use('/api/resources', resourceRoutes)
+app.use('/api/collection', collectionRoutes)
+app.use('/api/enterprises', enterprisesRoutes)
+app.use('/api/groups', communityGroupsRoutes)
+app.use('/api/calendar', calendarRoutes)
+app.use('/api/courses', courseRoutes)
+app.use('/api/news', newsRoutes)
+app.use('/api/categories', categoriesRoutes)
+app.use('/api/lessons', lessonsRoutes)
 
 // home page response
-app.get("/", (request, response) => {
-  response.json({ info: "Node.js, Express, and Postgres API on planetfarms" });
-});
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API on planetfarms' })
+})
 
 // Set static folder
-app.use(express.static(path.join(__dirname, "../files")));
-console.log(path.join(__dirname, "../files"));
+app.use(express.static(path.join(__dirname, '../files')))
+console.log(path.join(__dirname, '../files'))
 
 // port connection
 app.listen(PORT, () => {
-  console.log(`PlanetFarms is running on PORT ${PORT}.`);
-  console.log(`${process.env.DATABASE_HOST}`);
-});
+  console.log(`PlanetFarms is running on PORT ${PORT}.`)
+  console.log(`${process.env.DATABASE_HOST}`)
+})
 
 // database connection
 sequelize
   .authenticate()
-  .then(() => console.log("Connection has been established successfully."))
-  .catch((err) => console.log(`Error: ${err}`));
+  .then(() => console.log('Connection has been established successfully.'))
+  .catch((err) => console.log(`Error: ${err}`))
