bf5152ac657a14f6b8635146c0bae7fe1ac64c04
Puskar Adhikari <puskar.ad@gmail.com>
2021-06-22 13:31:38 +0545

    page redirect issue fixed

--- src/Screens/CourseManager/AdminCoursePage/LessonBlock.jsx
+++ src/Screens/CourseManager/AdminCoursePage/LessonBlock.jsx
@@ -1,38 +1,38 @@
-import { useHistory } from "react-router-dom";
+import { useHistory } from 'react-router-dom'
 
-const LessonBlock = () => {
-  const history = useHistory();
+const LessonBlock = ({ courseId }) => {
+  const history = useHistory()
   return (
-    <div className="admin-course-page-container">
-      <div className="admin-lesson-lists-container">
+    <div className='admin-course-page-container'>
+      <div className='admin-lesson-lists-container'>
         <h3>Lessons</h3>
-        <div className="admin-lesson">
+        <div className='admin-lesson'>
           <div>
-            <img src="/img/lesson-card2.svg" className="lesson image" />
+            <img src='/img/lesson-card2.svg' className='lesson image' />
             <div>
               <h4>01.Introducing</h4>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud{" "}
+                enim ad minim veniam, quis nostrud{' '}
               </p>
-              <button className="text-btn">See lesson</button>
+              <button className='text-btn'>See lesson</button>
             </div>
           </div>
-          <button className="secondary-btn lesson-btn">
-            <img src="/img/more-horizontal.svg" alt="horizontal icon" />
+          <button className='secondary-btn lesson-btn'>
+            <img src='/img/more-horizontal.svg' alt='horizontal icon' />
           </button>
         </div>
         <div
-          className="add-lesson-btn secondary-btn"
-          onClick={() => history.push("/admin/lesson-page")}
+          className='add-lesson-btn secondary-btn'
+          onClick={() => history.push(`/admin/add-lesson/${courseId}`)}
         >
-          <img src="/img/plus.svg" alt="lesson add" />
+          <img src='/img/plus.svg' alt='lesson add' />
           <span>Add new lesson for users</span>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default LessonBlock;
+export default LessonBlock
