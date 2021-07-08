a70694ae7788ed3db204338ab27b5dc2ae1b84e8
Laxman Maharjan <email@mlaxman.com.np>
2021-07-08 23:56:59 +0545

    fix migration conflict

--- api/src/migrations/20210627152703-alter-table-lessons-remove-column-steps.js
+++ api/src/migrations/20210627152703-alter-table-lessons-remove-column-steps.js
@@ -1,7 +1,6 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     queryInterface.removeColumn('lessons', 'steps')
-    queryInterface.removeColumn('lessons', 'courseId')
     queryInterface.addColumn('lessons', 'courseId', Sequelize.INTEGER)
     queryInterface.addConstraint('lessons', {
       fields: ['courseId'],
