fdc15d85853221ef11c4338df4ef09ce4429c692
Sujal Lama <lsujal41@gmail.com>
2021-07-09 17:56:38 +0545

    test api and ui implementation

--- src/actions/testActions.js
+++ src/actions/testActions.js
@@ -1,10 +1,11 @@
 import { TEST_CREATE_FAIL, TEST_CREATE_REQUEST, TEST_CREATE_SUCCESS, TEST_QUESTION_LIST_FAIL, TEST_QUESTION_LIST_REQUEST, TEST_QUESTION_LIST_SUCCESS } from '../constants/testConstants'
 import axios from 'axios'
+import { configFunc } from '../utils/apiFunc'
 
 export const createTest = (lessonId, questions) => async (dispatch, getState) => {
   try {
     dispatch({ type: TEST_CREATE_REQUEST })
-    console.log(lessonId, questions)
+    
     const { data } = await axios.post(`${process.env.REACT_APP_API_BASE_URL}/api/tests/add`, { lessonId, questions })
     dispatch({ type: TEST_CREATE_SUCCESS, payload: data })
   } catch (error) {
@@ -21,7 +22,8 @@ export const listTestQuestions = (testId) => async (
 ) => {
   try {
     // const {data: {tests: [test]}} = await axios.get( `${process.env.REACT_APP_API_BASE_URL}/api/tests/lesson/${lessonId}`);
-    const { data } = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/api/user_tests/${testId}`)
+    const config = configFunc();
+    const { data } = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/api/user_tests/${testId}`, config)
 
     dispatch({ type: TEST_QUESTION_LIST_REQUEST })
     const questions = await axios.get(
