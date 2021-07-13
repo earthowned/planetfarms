3df91684eac000c6bcaa22f157077848e84da569
Abhishek Raj Pandey <bhishek_rajpandey@yahoo.com>
2021-07-13 11:58:38 +0545

    added dispatch

--- src/components/privateRoute/PrivateRoute.jsx
+++ src/components/privateRoute/PrivateRoute.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Route, Redirect } from 'react-router-dom'
+import { checkAndUpdateToken } from '../../actions/userAction'
+import { useDispatch } from 'react-redux'
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
+  const dispatch = useDispatch();
   const hasAccess = () => {
-    return true
+    return dispatch(checkAndUpdateToken)
   }
   return (
-
-  // Show the component only when the user is logged in
-  // Otherwise, redirect the user to /signin page
     <Route
       {...rest} render={props => (
         hasAccess()
