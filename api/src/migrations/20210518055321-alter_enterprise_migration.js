34ac8e93ccee3dfc6d935e5db5d29a5d0debc7da
Laxman Maharjan <email@mlaxman.com.np>
2021-07-20 21:34:50 +0545

    clean blob

--- api/src/migrations/20210518055321-alter_enterprise_migration.js
+++ api/src/migrations/20210518055321-alter_enterprise_migration.js
@@ -1,11 +1,9 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    queryInterface.removeColumn('enterprises', 'attachments')
     queryInterface.addColumn('enterprises', 'filename', Sequelize.STRING)
   },
 
   down: async (queryInterface, Sequelize) => {
-    queryInterface.addColumn('enterprises', 'attachments', Sequelize.BLOB('long'))
     queryInterface.removeColumn('enterprises', 'filename')
   }
 }
