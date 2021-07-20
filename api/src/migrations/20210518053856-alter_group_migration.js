33f11db740971b17d54da16d38a99fab7c73f7c9
Laxman Maharjan <email@mlaxman.com.np>
2021-07-20 21:35:54 +0545

    remove blob

--- api/src/migrations/20210518053856-alter_group_migration.js
+++ api/src/migrations/20210518053856-alter_group_migration.js
@@ -1,11 +1,9 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    queryInterface.removeColumn('groups', 'attachments')
     queryInterface.addColumn('groups', 'filename', Sequelize.STRING)
   },
 
   down: async (queryInterface, Sequelize) => {
-    queryInterface.addColumn('groups', 'attachments', Sequelize.STRING)
     queryInterface.removeColumn('groups', 'filename')
   }
 }
