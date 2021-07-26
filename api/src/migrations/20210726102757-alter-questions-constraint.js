0860c6bea4f74177fef3cd44d39880680e94e1b6
Sujal Lama <lsujal41@gmail.com>
2021-07-26 17:20:14 +0545

    migration added to remove unique constraint

---
+++ api/src/migrations/20210726102757-alter-questions-constraint.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) =>  Promise.resolve()
+    .then(() => queryInterface.sequelize.query(`
+    ALTER TABLE "questions" DROP CONSTRAINT questions_question_key1;
+    ALTER TABLE "questions" DROP CONSTRAINT questions_question_key;
+    `))
+    .then(() => queryInterface.changeColumn(`questions`, `question`, {
+      type: Sequelize.TEXT,
+      unique: false,
+      allowNull: false,
+    })),
+
+  down: async (queryInterface, Sequelize) =>  Promise.resolve()
+    .then(() => queryInterface.sequelize.query(`
+    ALTER TABLE "questions" ADD CONSTRAINT questions_question_key UNIQUE (question);
+    `))
+    .then(() => queryInterface.changeColumn(`questions`, `question`, {
+    type: Sequelize.TEXT,
+    unique: true,
+    allowNull: false,
+  })),
+};
