fa10ae0237254f41a7c748ed80a7c6ecf0e88276
Sujal Lama <lsujal41@gmail.com>
2021-07-26 18:10:59 +0545

    permission based on role

---
+++ api/src/middleware/permission.js
@@ -0,0 +1,11 @@
+const permit = (role) => {
+  return (req, res, next) => {
+    if(req.user.role === role) {
+      next()
+    } else {
+      res.json({error: 'Sorry, You don\'t have permission'});
+    }
+  }
+}
+
+module.exports = permit
--- api/src/routes/categoriesRouter.js
+++ api/src/routes/categoriesRouter.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const router = express.Router()
-
+const protect = require('../middleware/authMiddleware')
 const {
   getCategories,
   addCategory,
   deleteCategory,
   updateCategory
 } = require('../controllers/categoryController')
+const permit = require('../middleware/permission')
 
-router.route('/').get(getCategories).post(addCategory)
-router.route('/:id').put(updateCategory).delete(deleteCategory)
+router.route('/').get(getCategories).post(protect, permit('sysadmin'), addCategory)
+router.route('/:id').put(protect, permit('sysadmin'), updateCategory).delete(protect, permit('sysadmin'), deleteCategory)
 
 module.exports = router
