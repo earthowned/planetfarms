5a1c47926cda50dc5a247b980ce601a49c14a326
Laxman Maharjan <email@mlaxman.com.np>
2021-07-26 21:54:56 +0545

    remove question unique

--- api/src/migrations/20210726102757-alter-questions-constraint.js
+++ api/src/migrations/20210726102757-alter-questions-constraint.js
@@ -1,24 +1,14 @@
 'use strict'
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => Promise.resolve()
-    .then(() => queryInterface.sequelize.query(`
-    ALTER TABLE "questions" DROP CONSTRAINT questions_question_key1;
-    ALTER TABLE "questions" DROP CONSTRAINT questions_question_key;
-    `))
-    .then(() => queryInterface.changeColumn('questions', 'question', {
-      type: Sequelize.TEXT,
-      unique: false,
-      allowNull: false
-    })),
-
-  down: async (queryInterface, Sequelize) => Promise.resolve()
-    .then(() => queryInterface.sequelize.query(`
-    ALTER TABLE "questions" ADD CONSTRAINT questions_question_key UNIQUE (question);
-    `))
-    .then(() => queryInterface.changeColumn('questions', 'question', {
-      type: Sequelize.TEXT,
-      unique: true,
-      allowNull: false
-    }))
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.removeConstraint('questions', 'questions_question_key')
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.addConstraint('questions', {
+      fields:  ['question'],
+      type: 'unique',
+      name: 'questions_question_key'
+    })
+  }
 }
