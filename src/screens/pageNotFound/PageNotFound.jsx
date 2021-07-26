b2b964a75d1c32eec6bed54f537fc711bc491138
Laxman Maharjan <email@mlaxman.com.np>
2021-07-26 23:17:08 +0545

    lint

--- src/screens/pageNotFound/PageNotFound.jsx
+++ src/screens/pageNotFound/PageNotFound.jsx
@@ -7,7 +7,7 @@ const PageNotFound = () => {
   const history = useHistory()
   return (
     <SignLayout>
-      <div class="sign">
+      <div class='sign'>
         <h1 className='welcome page-title'>404 Error</h1>
         <div className='container page-container'>
           <p className='page-sub-title'>Sorry! Page Not Found</p>
