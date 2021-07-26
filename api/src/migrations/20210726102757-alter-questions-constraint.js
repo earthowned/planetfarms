c9d4a3e01a697215e43d92fb8ce6c4f0b6ceca87
Sujal Lama <lsujal41@gmail.com>
2021-07-26 17:25:12 +0545

    js lint fix alter-questions constraint

--- api/src/migrations/20210726102757-alter-questions-constraint.js
+++ api/src/migrations/20210726102757-alter-questions-constraint.js
@@ -1,24 +1,24 @@
-'use strict';
+'use strict'
 
 module.exports = {
-  up: async (queryInterface, Sequelize) =>  Promise.resolve()
+  up: async (queryInterface, Sequelize) => Promise.resolve()
     .then(() => queryInterface.sequelize.query(`
     ALTER TABLE "questions" DROP CONSTRAINT questions_question_key1;
     ALTER TABLE "questions" DROP CONSTRAINT questions_question_key;
     `))
-    .then(() => queryInterface.changeColumn(`questions`, `question`, {
+    .then(() => queryInterface.changeColumn('questions', 'question', {
       type: Sequelize.TEXT,
       unique: false,
-      allowNull: false,
+      allowNull: false
     })),
 
-  down: async (queryInterface, Sequelize) =>  Promise.resolve()
+  down: async (queryInterface, Sequelize) => Promise.resolve()
     .then(() => queryInterface.sequelize.query(`
     ALTER TABLE "questions" ADD CONSTRAINT questions_question_key UNIQUE (question);
     `))
-    .then(() => queryInterface.changeColumn(`questions`, `question`, {
-    type: Sequelize.TEXT,
-    unique: true,
-    allowNull: false,
-  })),
-};
+    .then(() => queryInterface.changeColumn('questions', 'question', {
+      type: Sequelize.TEXT,
+      unique: true,
+      allowNull: false
+    }))
+}
