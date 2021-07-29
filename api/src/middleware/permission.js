085d573c9f09af2954a463a72fb43f3efd6b70a8
Sujal Lama <lsujal41@gmail.com>
2021-07-29 13:14:41 +0545

    multiple role adding to the middleware

--- api/src/middleware/permission.js
+++ api/src/middleware/permission.js
@@ -1,11 +1,21 @@
 const permit = (role) => {
   return (req, res, next) => {
-    if (req.user.role === role) {
+    
+    if (checkRole(req, role)) {
       next()
     } else {
       res.json({ error: 'Sorry, You don\'t have permission' })
     }
   }
+}
+
+function checkRole (req, role) {
+  let authorize = false;
+  role.forEach(el => {
+      if(el === req.user.role) return authorize = true;
+    })
+
+    return authorize;
 }
 
 module.exports = permit
