0ca05c00242ca24c3a7ebdc4dd4b4d0f582f3937
Sujal Lama <lsujal41@gmail.com>
2021-08-02 17:53:59 +0545

    news modal: same component for course and news

---
+++ src/components/btnCollection/BtnCollection.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import Text from '../../screens/courseManager/addLesson/Text'
+import Image from '../lessonImage/Image'
+import Video from '../videoPlayer/Video'
+
+const BtnCollection = ({data, setVideoModal, setImageModal, setTextModal}) => {
+    return (
+        <>
+            {data.length &&
+        data.map((vid, index) => (
+          <div key={index}>
+            <Video
+              title={vid.videoTitle}
+              description={vid.videoDescription}
+              url={vid.videoLink || vid.videoResource?.preview}
+              thumbnail={vid.videoCover?.preview}
+            />
+            <Image src={vid.lessonImg?.preview} desc={vid.photoDescription} />
+            <Text heading={vid.textHeading} desc={vid.textDescription} />
+          </div>
+        ))}
+      <div className='admin-lesson-create-btn-wrapper'>
+        <button className='secondary-btn' onClick={() => setVideoModal(true)}>
+          <img src='/img/video-outline.svg' alt='video icon' />{' '}
+          <span>Add video</span>
+        </button>
+        <button className='secondary-btn' onClick={() => setImageModal(true)}>
+          <img src='/img/image-outline.svg' alt='image_icon' />
+          <span>Add image</span>
+        </button>
+        <button className='secondary-btn' onClick={() => setTextModal(true)}>
+          <img src='/img/text-outline.svg' alt='text icon' />{' '}
+          <span>Add text</span>
+        </button>
+      </div>
+        </>
+    )
+}
+
+export default BtnCollection
