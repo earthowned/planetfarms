1699e8327c2bc8d24b9eae195f0cb520a8a5fe21
Laxman Maharjan <email@mlaxman.com.np>
2021-08-04 21:43:26 +0545

    remove fields from resources

---
+++ api/src/migrations/20210804154521-resource-remove-column.js
@@ -0,0 +1,121 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('resources', 'resourceFor')
+    await queryInterface.removeColumn('resources', 'publisher')
+    await queryInterface.removeColumn('resources', 'linkToLicense')
+    await queryInterface.removeColumn('resources', 'uploadDate')
+    await queryInterface.removeColumn('resources', 'addedBy')
+    await queryInterface.removeColumn('resources', 'openWith')
+    await queryInterface.removeColumn('resources', 'subject')
+    await queryInterface.removeColumn('resources', 'articleDate')
+    await queryInterface.removeColumn('resources', 'kind')
+    await queryInterface.removeColumn('resources', 'language')
+    await queryInterface.removeColumn('resources', 'author')
+    await queryInterface.removeColumn('resources', 'sum')
+    await queryInterface.removeColumn('resources', 'level')
+    await queryInterface.removeColumn('resources', 'languages')
+    await queryInterface.removeColumn('resources', 'timesRated')
+    await queryInterface.removeColumn('resources', 'tag')
+    await queryInterface.removeColumn('resources', 'year')
+    await queryInterface.removeColumn('resources', 'averageRating')
+    await queryInterface.removeColumn('resources', 'mediaType')
+    await queryInterface.removeColumn('resources', 'tags')
+    await queryInterface.removeColumn('resources', 'medium')
+    await queryInterface.removeColumn('resources', 'isDownloadable')
+    await queryInterface.removeColumn('resources', 'openUrl')
+    await queryInterface.removeColumn('resources', 'attachments')
+    await queryInterface.addColumn('resources', 'createdAt', {
+      type: Sequelize.DATE
+    })
+    await queryInterface.addColumn('resources', 'updatedAt', {
+      type: Sequelize.DATE
+    })
+    await queryInterface.removeColumn('resources', 'createdDate')
+    await queryInterface.removeColumn('resources', 'updatedDate')
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('resources', 'resourceFor', {
+      type: Sequelize.TEXT
+    })
+    await queryInterface.addColumn('resources', 'publisher', {
+      type: Sequelize.STRING
+    })
+    await queryInterface.addColumn('resources', 'linkToLicense', {
+      type: Sequelize.STRING
+    })
+    await queryInterface.addColumn('resources', 'uploadDate', {
+      type: Sequelize.DATE
+    })
+    await queryInterface.addColumn('resources', 'addedBy', {
+      type: Sequelize.STRING
+    })
+    await queryInterface.addColumn('resources', 'openWith', {
+      type: Sequelize.STRING
+    })
+    await queryInterface.addColumn('resources', 'subject', {
+      type: Sequelize.TEXT
+    })
+    await queryInterface.addColumn('resources', 'articleDate', {
+      type: Sequelize.DATE
+    })
+    await queryInterface.addColumn('resources', 'kind', {
+      type: Sequelize.STRING
+    })
+    await queryInterface.addColumn('resources', 'language', {
+      type: Sequelize.STRING
+    })
+    await queryInterface.addColumn('resources', 'author', {
+      type: Sequelize.STRING
+    })
+    await queryInterface.addColumn('resources', 'sum', {
+      type: Sequelize.DOUBLE
+    })
+    await queryInterface.addColumn('resources', 'level', {
+      type: Sequelize.TEXT
+    })
+    await queryInterface.addColumn('resources', 'languages', {
+      type: Sequelize.TEXT
+    })
+    await queryInterface.addColumn('resources', 'timesRated', {
+      type: Sequelize.STRING
+    })
+    await queryInterface.addColumn('resources', 'tag', {
+      type: Sequelize.TEXT
+    })
+    await queryInterface.addColumn('resources', 'year', {
+      type: Sequelize.DATE
+    })
+    await queryInterface.addColumn('resources', 'averageRating', {
+      type: Sequelize.DOUBLE
+    })
+    await queryInterface.addColumn('resources', 'mediaType', {
+      type: Sequelize.STRING
+    })
+    await queryInterface.addColumn('resources', 'tags', {
+      type: Sequelize.STRING
+    })
+    await queryInterface.addColumn('resources', 'medium', {
+      type: Sequelize.STRING
+    })
+    await queryInterface.addColumn('resources', 'isDownloadable', {
+      type: Sequelize.BOOLEAN
+    })
+    await queryInterface.addColumn('resources', 'openUrl', {
+      type: Sequelize.STRING
+    })
+    await queryInterface.addColumn('resources', 'createdDate', {
+      type: Sequelize.DATE
+    })
+    await queryInterface.addColumn('resources', 'updatedDate', {
+      type: Sequelize.DATE
+    })
+    await queryInterface.addColumn('resources', 'attachments', {
+      type: Sequelize.TEXT
+    })
+    await queryInterface.removeColumn('resources', 'createdAt')
+    await queryInterface.removeColumn('resources', 'updatedAt')
+  }
+};
