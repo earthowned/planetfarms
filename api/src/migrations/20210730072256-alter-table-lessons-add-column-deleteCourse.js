ca9793edddfae578aa7749d2a0e772030897f8fc
Laxman Maharjan <email@mlaxman.com.np>
2021-08-05 21:43:16 +0545

    migrationg

--- api/src/migrations/20210730072256-alter-table-lessons-add-column-deleteCourse.js
+++ api/src/migrations/20210730072256-alter-table-lessons-add-column-deleteCourse.js
@@ -1,6 +1,6 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.removeConstraint('lessons', 'courseId_fkey_forLessons')
+    await queryInterface.removeConstraint('lessons', 'courseId_fkey_forLesson')
     await queryInterface.addConstraint('lessons', {
       fields: ['courseId'],
       type: 'foreign key',
@@ -18,7 +18,7 @@ module.exports = {
     await queryInterface.addConstraint('lessons', {
       fields: ['courseId'],
       type: 'foreign key',
-      name: 'courseId_fkey_forLessons',
+      name: 'courseId_fkey_forLesson',
       references: {
         table: 'courses',
         field: 'id'
