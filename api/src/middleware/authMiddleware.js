c735ad098eac1b02f0aabc20e89b152cbc48f34f
rjpadilla <23529006+rjpadilla@users.noreply.github.com>
2021-08-05 00:00:23 -0400

    first step in refactoring authMiddleware

--- api/src/middleware/authMiddleware.js
+++ api/src/middleware/authMiddleware.js
@@ -2,43 +2,36 @@ const jwt = require('jsonwebtoken')
 const jwkToPem = require('jwk-to-pem')
 const db = require('../models')
 
-module.exports = async (req, res, next) => {
-  let decoded
-  let recoded
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith('Bearer')
-  ) {
-    try {
-      const token = req.headers.authorization.split(' ')[1]
-      if (process.env.AUTH_METHOD !== 'cognito') {
-        jwt.verify(token, process.env.JWT_SECRET, function (err, decodedToken) {
-          if (err) {
-            if (err.message === 'jwt expired') {
-              throw Error('TokenExpired')
-            } else {
-              throw Error('InvalidToken')
-            }
-          }
-          decoded = decodedToken
-        })
-      } else {
-        const jwk = require('./jwks.json')
-        const pem = jwkToPem(jwk.keys[0])
-        jwt.verify(token, pem, { algorithms: ['RS256'] }, function (err, decodedToken) {
-          if (err) {
-            if (err.message === 'jwt expired') {
-              throw Error('TokenExpired')
-            } else {
-              throw Error('InvalidToken')
-            }
-          }
-          recoded = decodedToken
-        })
-      }
-      /*
-      * TODO: Maintain session and check again local session
-      */
+const responses = [
+  {
+    error: 'Invalid token provided.',
+    name: 'InvalidToken'
+  },
+  {
+    error: 'The token has been expired.',
+    name: 'TokenExpired'
+  },
+  {
+    error: 'Not authorized, token failed',
+    name: 'Unauthorized'
+  },
+  {
+    error: 'Unauthorized',
+    name: 'Unauthorized'
+  }
+]
+
+function checkAuthorization(err) {
+  if (err.message === 'jwt expired') {
+    throw Error('TokenExpired')
+  } else {
+    throw Error('InvalidToken')
+  }
+}
+
+const throwError = message => responses.filter(response => response.name.match(message))
+
+function maintainState() {
       if (process.env.AUTH_METHOD !== 'cognito') {
         req.user = await db.User.findOne({ where: { userID: decoded.id } })
       } else if (recoded) {
@@ -46,33 +39,48 @@ module.exports = async (req, res, next) => {
       } else {
         throw Error('User not found')
       }
+
+}
+
+const recode = (err, decodedToken) => {
+  if (err) {
+   checkAuthorization(err) 
+  }
+  recoded = decodedToken
+}
+
+const decode = (err, decodedToken) => {
+  if (err) {
+   checkAuthorization(err) 
+  }
+  decoded = decodedToken
+}
+
+module.exports = async (req, res, next) => {
+  let decoded
+  let recoded
+  const headers = req.headers.authorization && req.headers.authorization.startsWith('Bear')
+  if (
+    headers
+  ) {
+    try {
+      const token = req.headers.authorization.split(' ')[1]
+      if (process.env.AUTH_METHOD !== 'cognito') {
+        jwt.verify(token, process.env.JWT_SECRET, decode(err, decodedToken))
+      } else {
+        const jwk = require('./jwks.json')
+        const pem = jwkToPem(jwk.keys[0])
+        jwt.verify(token, pem, { algorithms: ['RS256'] }, recode(err, decodedToken))
+      }
+      /*
+      * TODO: Maintain session and check again local session
+      */
+      maintainState()
       next()
     } catch (error) {
-      switch (error.message) {
-        case 'InvalidToken':
-          res.status(401).json({
-            error: 'Invalid token provided.',
-            name: error.message
-          })
-          break
-        case 'TokenExpired':
-          res.status(401).json({
-            error: 'The token has been expired.',
-            name: error.message
-          })
-          break
-        default:
-          res.status(401).json({
-            error: 'Not authorized, token failed',
-            name: 'Unauthorized'
-          })
-          break
-      }
+      res.status(401).json(throwError(error.message))      
     }
   } else {
-    res.status(401).json({
-      error: 'Unauthorized',
-      name: 'Unauthorized'
-    })
+    res.status(401).json(throwError('Unauthorized'))
   }
 }
