e9c9febea1e7e174d4b898d8d57c1188f3b56035
rjpadilla <23529006+rjpadilla@users.noreply.github.com>
2021-08-09 11:44:34 -0400

    adding back quotes??

--- api/src/middleware/authMiddleware.js
+++ api/src/middleware/authMiddleware.js
@@ -6,20 +6,20 @@ let coded
 
 const responses = [
   {
-    error: 'Invalid token provided.',
-    name: 'InvalidToken'
+    'error': 'Invalid token provided.',
+    'name': 'InvalidToken'
   },
   {
-    error: 'The token has been expired.',
-    name: 'TokenExpired'
+    'error': 'The token has been expired.',
+    'name': 'TokenExpired'
   },
   {
-    error: 'Not authorized, token failed',
-    name: 'Unauthorized'
+    'error': 'Not authorized, token failed',
+    'name': 'Unauthorized'
   },
   {
-    error: 'Unauthorized',
-    name: 'Unauthorized'
+    'error': 'Unauthorized',
+    'name': 'Unauthorized'
   }
 ]
 
