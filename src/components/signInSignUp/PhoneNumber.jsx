7c4f9b369f9be293b5bbb6f8b15cff8a7324977f
Puskar Adhikari <puskar.ad@gmail.com>
2021-08-11 17:57:31 +0545

    styling

--- src/components/signInSignUp/PhoneNumber.jsx
+++ src/components/signInSignUp/PhoneNumber.jsx
@@ -3,6 +3,7 @@ import PhoneInput from 'react-phone-number-input'
 const PhoneNumber = ({ value, setValue }) => {
   return (
     <PhoneInput
+      className='signInput'
       international
       defaultCountry='US'
       value={value}
