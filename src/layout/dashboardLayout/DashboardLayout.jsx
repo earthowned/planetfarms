772b38e58a14bef73fa0ac5ede3e3212536c1d1b
rjpadilla <23529006+rjpadilla@users.noreply.github.com>
2021-08-16 11:48:29 -0400

    Revert "fixed invalid DOM propertry warning"
    This reverts commit 5a5aeb1dcb6e5f387ac9b9527a5221a9307158b7.

--- src/layout/dashboardLayout/DashboardLayout.jsx
+++ src/layout/dashboardLayout/DashboardLayout.jsx
@@ -139,9 +139,9 @@ const DashboardLayout = ({ title, children }) => {
 function Hamburger () {
   return (
     <svg className='ham-tab' width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
-      <path d='M3 12H21' stroke='#00C688' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round' />
-      <path d='M3 6H21' stroke='#00C688' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round' />
-      <path d='M3 18H21' stroke='#00C688' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round' />
+      <path d='M3 12H21' stroke='#00C688' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' />
+      <path d='M3 6H21' stroke='#00C688' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' />
+      <path d='M3 18H21' stroke='#00C688' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' />
     </svg>
   )
 }
