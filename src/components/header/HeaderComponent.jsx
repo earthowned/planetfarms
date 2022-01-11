6bd7e1dcb941fe2bdb728259dcef6b0e2d5b4b5c
Alexander <alexander@sweetcode.pro>
2022-01-12 01:49:20 +0200

    PF-2 update styles in header

--- src/components/header/HeaderComponent.jsx
+++ src/components/header/HeaderComponent.jsx
@@ -1,69 +1,104 @@
-import { useState } from 'react'
-import MessageDropdown from '../messageDropdown/MessageDropdown'
-import './HeaderComponent.css'
-import { useHistory } from 'react-router-dom'
-import ProfileMenu from './ProfileMenu'
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+
+import ProfileMenu from "./ProfileMenu";
+import MessageDropdown from "../messageDropdown/MessageDropdown";
+
+import "./HeaderComponent.css";
+
+const ListContainer = ({ onSwitchCommunity, children }) => {
+  return (
+    <ul>
+      <li className="community-switch-btn" onClick={onSwitchCommunity}>
+        <button>Switch Community</button>
+      </li>
+      {children}
+    </ul>
+  );
+};
+
+const ListItem = ({
+  onClick,
+  title = "",
+  badge = undefined,
+  iconName = undefined,
+  children = undefined,
+}) => {
+  return (
+    <li className="header-tab-li" onClick={onClick}>
+      {iconName && (
+        <img src={`/img/${iconName}.svg`} alt={`${iconName}-icon`} />
+      )}
+      {title && <h4>{title}</h4>}
+      {badge && <div className="badge">{badge}</div>}
+      {children && children}
+    </li>
+  );
+};
 
 const HeaderComponent = ({ title }) => {
-  const [active, setActive] = useState(false)
-  const [notification, setNotification] = useState(false)
+  const history = useHistory();
 
-  function messageNoti () {
-    setActive(!active)
-    setNotification(false)
-  }
+  const [showMessages, setShowMessages] = useState(false);
+  const [showNotifications, setShowNotifications] = useState(false);
 
-  function notificationNoti () {
-    setNotification(!notification)
-    setActive(false)
-  }
-  const history = useHistory()
+  const onNotificationsClick = () => {
+    setShowMessages(false);
+    setShowNotifications(!showNotifications);
+  };
 
-  function handleClick () {
-    history.push('/messenger')
-  }
+  const onMessagesClick = () => {
+    setShowMessages(!showMessages);
+    setShowNotifications(false);
+  };
 
   return (
     <>
-      <header className='header-container'>
+      <header className="header-container">
         <h3>{title}</h3>
-        <ul>
-          <li className='community-switch-btn' onClick={() => history.push('/community-switching')}><button>Switch Community</button></li>
-          <li style={{ cursor: 'pointer' }} onClick={() => history.push('/dashboard')}>
-            <img src='/img/user-green-outline.svg' alt='user-green-outline' />
-            <span>My dashboard</span>
-          </li>
-          <li onClick={() => messageNoti()} className='message'>
-            <img src='/img/email.svg' alt='email-icon' />
-            {active || <div className='message-noti'>6</div>}
-            {active && (
-              <MessageDropdown
-                clickHandler={setActive}
-                message='Your messages'
-                btnName='See all messages'
-                handleClick={handleClick}
-              />
-            )}
-          </li>
-          <li onClick={() => notificationNoti()} className='message'>
-            <img src='/img/bell-icon.svg' alt='bell-icon' />
-            {notification && (
-              <MessageDropdown
-                clickHandler={setNotification}
-                message='Your notifications'
-                btnName='See all notifications'
-                handleClick={handleClick}
-              />
-            )}
-          </li>
 
-          <li className='message'>
+        <ListContainer
+          onSwitchCommunity={() => history.push("/community-switching")}
+        >
+          <ListItem
+            title="My dashboard"
+            iconName="user-green-outline"
+            onClick={() => history.push("/dashboard")}
+          />
+
+          <ListItem badge={6} iconName="email" onClick={onMessagesClick}>
+            {showMessages && (
+              <MessageDropdown
+                message="Your messages"
+                btnName="See all messages"
+                clickHandler={setShowMessages}
+                handleClick={() => history.push("/messenger")}
+              />
+            )}
+          </ListItem>
+
+          <ListItem
+            badge={4}
+            iconName="bell-icon"
+            onClick={onNotificationsClick}
+          >
+            {showNotifications && (
+              <MessageDropdown
+                message="Your notifications"
+                btnName="See all notifications"
+                clickHandler={setShowNotifications}
+                handleClick={() => history.push("/messenger")}
+              />
+            )}
+          </ListItem>
+
+          <ListItem>
             <ProfileMenu />
-          </li>
-        </ul>
+          </ListItem>
+        </ListContainer>
       </header>
     </>
-  )
-}
+  );
+};
 
-export default HeaderComponent
+export default HeaderComponent;
