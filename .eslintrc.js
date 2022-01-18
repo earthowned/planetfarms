46390779f3d9a1ffbed2b650c787ba7a3dc8cd22
ZheleznovTN <43418944+ZheleznovTN@users.noreply.github.com>
2022-01-18 14:15:24 +0200

    eslint config (#466)
    Co-authored-by: Alexander <alexander@sweetcode.pro>

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  parser: "@babel/eslint-parser",
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "airbnb",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+  ],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2021,
+    sourceType: "module",
+    requireConfigFile: false,
+  },
+  plugins: ["only-warn", "react", "prettier"],
+  // ignorePatterns: ["styles.js"],
+  rules: {
+    "prettier/prettier": ["warn"],
+    "arrow-parens": [2, "always"],
+    "react/function-component-definition": "off",
+    "react/jsx-wrap-multilines": 0,
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": [2, { extensions: [".js", ".jsx"] }],
+    "import/no-extraneous-dependencies": 0,
+    "import/no-cycle": 0,
+    camelcase: 0,
+    "react/jsx-props-no-spreading": 0,
+    "import/no-unresolved": 0,
+    "import/extensions": 0,
+    "import/prefer-default-export": "off",
+    "no-param-reassign": ["error", { props: false }],
+    "guard-for-in": 0,
+    "no-unused-vars": ["warn"],
+    "ban-ts-comment": "off",
+    "react/prop-types": "off",
+  },
+};
