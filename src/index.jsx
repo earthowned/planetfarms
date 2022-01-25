dce4bdeae1017c67702203c1ed644d37fb25c295
ZheleznovTN <43418944+ZheleznovTN@users.noreply.github.com>
2022-01-25 14:50:11 +0200

    use local fonts (#478)
    Co-authored-by: dogi <dogi@users.noreply.github.com>

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import "./index.css";
+
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
 import store from "./store";
+
+import "./index.css";
+import "./scss/styles.scss";
 
 ReactDOM.render(
   <Provider store={store}>
