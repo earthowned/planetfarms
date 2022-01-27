e47b7e059b810d47c576f7c5a78e2d066e9afaed
Alexander <alexander@sweetcode.pro>
2022-01-28 01:18:16 +0200

    PF-25 minor layout fixes

--- src/layout/auth-layout/index.jsx
+++ src/layout/auth-layout/index.jsx
@@ -8,18 +8,19 @@ import "./styles.scss";
 export const AuthLayout = ({ title, children }) => {
   return (
     <div className="auth-container">
-      <div className="form-container">
-        <div className="header">
-          <Icon icon="logo-small" />
-        </div>
-
-        <h1>{title}</h1>
-
-        {children}
+      <div className="header">
+        <Icon icon="logo-small" />
       </div>
 
-      <div className="big-logo-container">
-        <img src={BigLogoImage} alt="big-logo" />
+      <div className="content-container">
+        <div className="form-container">
+          <h1>{title}</h1>
+          {children}
+        </div>
+
+        <div className="big-logo-container">
+          <img src={BigLogoImage} alt="big-logo" />
+        </div>
       </div>
     </div>
   );
