dd85659cf5e2e9dbbc0180bd05ffb76751836480
Alexander <alexander@sweetcode.pro>
2022-02-18 04:30:05 +0200

    PF-63 fix mobile scrolling bugs

--- src/layout/blur-container/config.js
+++ src/layout/blur-container/config.js
@@ -1,3 +1,1 @@
-export const circles = ["top", "right", "bottom", "left"];
-
 export const Variant = { auth: "auth", dashboard: "dashboard" };
--- src/layout/blur-container/index.jsx
+++ src/layout/blur-container/index.jsx
@@ -1,33 +1,18 @@
 import { useMemo } from "react";
 import cx from "classnames";
 
-import { circles, Variant } from "./config";
+import { Variant } from "./config";
 
 import "./styles.scss";
 
-const BlurCircle = ({ position }) => {
-  const className = cx("blur-circle", { [`blur-circle-${position}`]: true });
-  return <div className={className} />;
-};
-
 export const BlurContainer = ({ children, variant = Variant.dashboard }) => {
-  const containerClassName = useMemo(
+  const className = useMemo(
     () =>
-      cx("blur-circles-container", {
-        [`blur-circles-container-${variant}`]: true,
+      cx("blur-page-container", {
+        [`blur-page-container-${variant}`]: true,
       }),
     [variant]
   );
 
-  return (
-    <div className="blur-page-container">
-      <div className={containerClassName}>
-        {circles.map((position) => (
-          <BlurCircle key={`blur-circle-${position}`} position={position} />
-        ))}
-      </div>
-
-      <div className="page-container">{children}</div>
-    </div>
-  );
+  return <div className={className}>{children}</div>;
 };
