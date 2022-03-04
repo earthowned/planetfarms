10b024e9ed3461009cb1ce7ba073adae5866807d
Alexander <alexander@sweetcode.pro>
2022-03-04 23:56:47 +0200

    implement news item and news layout

---
+++ src/common/list-items/news-item/index.jsx
@@ -0,0 +1,53 @@
+import cx from "classnames";
+
+import { Avatar } from "common/avatar";
+
+import "./styles.scss";
+
+export const NewsItem = ({ variant, news }) => {
+  const containerClassName = cx("news-item-container", {
+    [`news-item-container-${variant}`]: true,
+  });
+
+  return (
+    <div className={containerClassName}>
+      <div className="cover-image-container">
+        <img
+          src={news.image}
+          alt=""
+          style={{ width: "100%", height: "100%" }}
+        />
+      </div>
+
+      <div className="news-data-container">
+        <div className="top-container">
+          {news.tag && <h6>{news.tag}</h6>}
+
+          {news.createdAt && (
+            <div className="dot-container">
+              <div className="dot" />
+              <h6 className="grey">{news.createdAt}</h6>
+            </div>
+          )}
+
+          {news.readTime && (
+            <div className="dot-container">
+              <div className="dot" />
+              <h6 className="grey">{news.readTime}</h6>
+            </div>
+          )}
+        </div>
+
+        {variant === "default" && <h3>{news.title}</h3>}
+        {variant === "big" && <h2>{news.title}</h2>}
+
+        <h5>{news.smallText}</h5>
+
+        <div className="news-author-container">
+          <Avatar placeholderIcon="person" />
+          <h4>{news.authorName}</h4>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/screens/news/news-list/index.jsx
+++ src/screens/news/news-list/index.jsx
@@ -1,12 +1,160 @@
 import { useState } from "react";
 
 import { DashboardLayout } from "layout/dashboard";
+import { NewsItem } from "common/list-items/news-item";
 import { ActionButton } from "common/buttons/action-button";
 
 import { Filters } from "./filters";
 import { FiltersModal } from "./filters-modal";
 
 import "./styles.scss";
+
+const newsList = [
+  {
+    id: 0,
+    image:
+      "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg",
+    title: "Fueling the ethanol industry",
+    smallText:
+      "Ethanol, which is sometimes known as ethyl alcohol, is a kind of alcohol derived from corn, sugarcane, and grain or indirectly from paper waste. It’s also the...",
+    createdAt: "November 01, 2020",
+    readTime: "5 min read",
+    tag: "Farming",
+    authorName: "Leslie Alexander",
+  },
+  {
+    id: 1,
+    image:
+      "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg",
+    title: "Fueling the ethanol industry",
+    smallText:
+      "Ethanol, which is sometimes known as ethyl alcohol, is a kind of alcohol derived from corn, sugarcane, and grain or indirectly from paper waste. It’s also the...",
+    createdAt: "November 01, 2020",
+    readTime: "5 min read",
+    tag: "Farming",
+    authorName: "Leslie Alexander",
+  },
+  {
+    id: 2,
+    image:
+      "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg",
+    title: "Fueling the ethanol industry",
+    smallText:
+      "Ethanol, which is sometimes known as ethyl alcohol, is a kind of alcohol derived from corn, sugarcane, and grain or indirectly from paper waste. It’s also the...",
+    createdAt: "November 01, 2020",
+    readTime: "5 min read",
+    tag: "Farming",
+    authorName: "Leslie Alexander",
+  },
+  {
+    id: 3,
+    image:
+      "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg",
+    title: "Fueling the ethanol industry",
+    smallText:
+      "Ethanol, which is sometimes known as ethyl alcohol, is a kind of alcohol derived from corn, sugarcane, and grain or indirectly from paper waste. It’s also the...",
+    createdAt: "November 01, 2020",
+    readTime: "5 min read",
+    tag: "Farming",
+    authorName: "Leslie Alexander",
+  },
+  {
+    id: 4,
+    image:
+      "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg",
+    title: "Fueling the ethanol industry",
+    smallText:
+      "Ethanol, which is sometimes known as ethyl alcohol, is a kind of alcohol derived from corn, sugarcane, and grain or indirectly from paper waste. It’s also the...",
+    createdAt: "November 01, 2020",
+    readTime: "5 min read",
+    tag: "Farming",
+    authorName: "Leslie Alexander",
+  },
+  {
+    id: 5,
+    image:
+      "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg",
+    title: "Fueling the ethanol industry",
+    smallText:
+      "Ethanol, which is sometimes known as ethyl alcohol, is a kind of alcohol derived from corn, sugarcane, and grain or indirectly from paper waste. It’s also the...",
+    createdAt: "November 01, 2020",
+    readTime: "5 min read",
+    tag: "Farming",
+    authorName: "Leslie Alexander",
+  },
+  {
+    id: 6,
+    image:
+      "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg",
+    title: "Fueling the ethanol industry",
+    smallText:
+      "Ethanol, which is sometimes known as ethyl alcohol, is a kind of alcohol derived from corn, sugarcane, and grain or indirectly from paper waste. It’s also the...",
+    createdAt: "November 01, 2020",
+    readTime: "5 min read",
+    tag: "Farming",
+    authorName: "Leslie Alexander",
+  },
+  {
+    id: 7,
+    image:
+      "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg",
+    title: "Fueling the ethanol industry",
+    smallText:
+      "Ethanol, which is sometimes known as ethyl alcohol, is a kind of alcohol derived from corn, sugarcane, and grain or indirectly from paper waste. It’s also the...",
+    createdAt: "November 01, 2020",
+    readTime: "5 min read",
+    tag: "Farming",
+    authorName: "Leslie Alexander",
+  },
+  {
+    id: 8,
+    image:
+      "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg",
+    title: "Fueling the ethanol industry",
+    smallText:
+      "Ethanol, which is sometimes known as ethyl alcohol, is a kind of alcohol derived from corn, sugarcane, and grain or indirectly from paper waste. It’s also the...",
+    createdAt: "November 01, 2020",
+    readTime: "5 min read",
+    tag: "Farming",
+    authorName: "Leslie Alexander",
+  },
+  {
+    id: 9,
+    image:
+      "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg",
+    title: "Fueling the ethanol industry",
+    smallText:
+      "Ethanol, which is sometimes known as ethyl alcohol, is a kind of alcohol derived from corn, sugarcane, and grain or indirectly from paper waste. It’s also the...",
+    createdAt: "November 01, 2020",
+    readTime: "5 min read",
+    tag: "Farming",
+    authorName: "Leslie Alexander",
+  },
+  {
+    id: 10,
+    image:
+      "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg",
+    title: "Fueling the ethanol industry",
+    smallText:
+      "Ethanol, which is sometimes known as ethyl alcohol, is a kind of alcohol derived from corn, sugarcane, and grain or indirectly from paper waste. It’s also the...",
+    createdAt: "November 01, 2020",
+    readTime: "5 min read",
+    tag: "Farming",
+    authorName: "Leslie Alexander",
+  },
+  {
+    id: 11,
+    image:
+      "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg",
+    title: "Fueling the ethanol industry",
+    smallText:
+      "Ethanol, which is sometimes known as ethyl alcohol, is a kind of alcohol derived from corn, sugarcane, and grain or indirectly from paper waste. It’s also the...",
+    createdAt: "November 01, 2020",
+    readTime: "5 min read",
+    tag: "Farming",
+    authorName: "Leslie Alexander",
+  },
+];
 
 export const NewsListPage = () => {
   const [filters, setFilters] = useState([]);
@@ -21,6 +169,11 @@ export const NewsListPage = () => {
     setAddFilterVisible(false);
   };
 
+  const getNewsItemVariant = (index) => {
+    if (index === 0 || index % 4 === 0) return "big";
+    return "default";
+  };
+
   return (
     <DashboardLayout title="News">
       <div className="news-page-header">
@@ -33,7 +186,15 @@ export const NewsListPage = () => {
         <ActionButton variant="primary" title="Add New" icon="plus" />
       </div>
 
-      <div className="news-list-container" />
+      <div className="news-list-container">
+        {newsList.map((item, index) => (
+          <NewsItem
+            news={item}
+            key={item.id}
+            variant={getNewsItemVariant(index)}
+          />
+        ))}
+      </div>
 
       <FiltersModal
         selectedFilters={filters}
