b80920d2fec2d2ecbe87145605e21c0362ca3eb1
Alexander <alexander@sweetcode.pro>
2022-03-16 01:58:46 +0200

    implement text area component

---
+++ src/common/text-area/index.jsx
@@ -0,0 +1,30 @@
+import TextareaAutosize from "react-textarea-autosize";
+
+import "./styles.scss";
+
+export const TextArea = ({
+  name,
+  value,
+  maxLength,
+  placeholder,
+  onChangeValue,
+  minHeight = "72px",
+}) => {
+  return (
+    <div className="text-area-container" style={{ minHeight }}>
+      <TextareaAutosize
+        name={name}
+        value={value}
+        maxLength={maxLength}
+        placeholder={placeholder}
+        onChange={(e) => onChangeValue(e.target.value)}
+      />
+
+      {maxLength && (
+        <div className="characters-count-container">
+          <h6>{`${value.length}/${maxLength}`}</h6>
+        </div>
+      )}
+    </div>
+  );
+};
