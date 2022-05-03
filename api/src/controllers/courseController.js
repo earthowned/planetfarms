f4158d1b1606e2db1d02fa66e01a759407d1d1e9
Neil Palima <neil.palima@sweetcode.pro>
2022-05-03 14:19:53 +0800

    courses api updates

--- api/src/controllers/courseController.js
+++ api/src/controllers/courseController.js
@@ -1,11 +1,13 @@
+const { Joi } = require('express-validation')
 const Sequelize = require('sequelize')
 const Op = Sequelize.Op
 const { sequelize } = require('../models')
 const db = require('../models')
 const NotFoundError = require('../errors/notFoundError')
-const CircularJSON = require('circular-json')
 const { changeFormat } = require('../helpers/filehelpers')
 const { paginatedResponse } = require('../utils/query')
+const BadRequestError = require('../errors/badRequestError')
+const ForbiddenRequestError = require('../errors/forbiddenRequestError')
 
 // @desc    Fetch all course
 // @route   GET /api/courses?pageNumber=${pageNumber}&category=${category}&search=${search}
@@ -17,7 +19,7 @@ const getCourses = async (req, res) => {
   enrollQuery = sequelize.literal(`(SELECT COUNT("userId") FROM enrolls WHERE "courseId" = courses.id)`)
   switch (filter) {
     case 'My':
-      filterBy = { creator: { [Op.eq]: ''+req.user.userID+'' } }
+      filterBy = { creatorId: req.user.id }
       break;
     case 'Paid':
       hasPurchase = true
@@ -50,11 +52,16 @@ const getCourses = async (req, res) => {
     offset: (pageNumber - 1) * pageSize,
     limit: pageSize,
     order: [order],
-    attributes: ['id', 'title', 'description', 'creator', 'thumbnail', 'price', 'isFree', [enrollQuery, 'members']],
+    attributes: ['id', 'title', 'description', 'creatorId', 'thumbnail', 'price', 'isFree', [enrollQuery, 'members']],
     include: [
       db.Lesson,
       db.Category,
       db.CourseView,
+      {
+        model: db.User,
+        as: 'creator',
+        attributes: ['id', 'userID', 'email', 'firstName', 'lastName', 'attachments'],
+      },
       {
         model: db.User,
         as: 'enrolledUser',
@@ -71,22 +78,48 @@ const getCourses = async (req, res) => {
       },
     ],
     where: {
-      ...(search ? { title: { [Op.iLike]: '%' + search + '%' } } : {}),
+      ...(search ? { title: { [Op.iLike]: `%${search}%` } } : {}),
       ...filterBy
     },
     distinct: true
   })
-  courses.rows.forEach((course) => {
-    course.thumbnail = changeFormat(course.thumbnail)
-    course.lessons.forEach((lesson) => {
-      lesson.coverImg = changeFormat(lesson.coverImg)
-    })
-  })
+
+  // TODO: need to fix resizing of image when used with multiple files
+  // courses.rows.forEach((course) => {
+  //   course.thumbnail = changeFormat(course.thumbnail)
+  //   course.lessons.forEach((lesson) => {
+  //     lesson.coverImg = changeFormat(lesson.coverImg)
+  //   })
+  // })
   res.status(200).json({
-    status: true,
-    message: 'Fetched successfully',
+    message: 'Fetched courses successfully',
     ...paginatedResponse({ data: courses, pageSize, pageNumber })
   })
+}
+
+const courseSchema = {
+  body: Joi.object({
+    title: Joi.string().required(),
+    price: Joi.number().positive(),
+    isPublished: Joi.boolean().required(),
+    categoryId: Joi.string().required(),
+    text_heading: Joi.alternatives().try(
+      Joi.string(),
+      Joi.array().items(Joi.string())
+    ),
+    text_description: Joi.alternatives().try(
+      Joi.string(),
+      Joi.array().items(Joi.string())
+    ),
+    photo_description: Joi.alternatives().try(
+      Joi.string(),
+      Joi.array().items(Joi.string())
+    ),
+    order: Joi.alternatives().try(
+      Joi.string().valid('text', 'image'),
+      Joi.array().items(Joi.string().valid('text', 'image'))
+    ).required()
+  }),
 }
 
 // @desc    Add individual course
@@ -94,18 +127,80 @@ const getCourses = async (req, res) => {
 // @access  Public
 const addCourse = async (req, res) => {
   try {
-    let thumbnail = ''
-    if (req.file) {
-      thumbnail = req.file.filename
+    const { body, user, files } = req
+    const thumbnail = files.thumbnail?.[0]?.filename || null
+    const price = body.price > 0 ? Number(body.price) : null
+
+    const courseData = {
+      title: body.title,
+      description: body.description,
+      thumbnail: thumbnail ? `thumbnail/${thumbnail}` : null,
+      price,
+      isPublished: body.isPublished,
+      isFree: price === null ? true : false,
+      creatorId: user.id,
+      categoryId: body.categoryId
     }
-    const course = await db.Courses.create({ ...req.body, thumbnail })
+    let createdCourseId;
+
+    await db.sequelize.transaction(async (transaction) => {
+      let richtext = null
+      let richtextId = null
+
+      if (body.order) {
+        richtext = await db.RichText.create({}, { transaction })
+        richtextId = richtext.id
+
+        const richtextCount = Array.isArray(body.text_description) ? body.text_description.length : 1
+        const photoCount = files.courses?.length || 0
+        const orderCount = Array.isArray(body.order) ? body.order.length : 1
+
+        // add validation if texts and photo tallies the order count
+        if (photoCount + richtextCount !== orderCount) {
+          throw new BadRequestError('Contents don\'t match the ordering count')
+        }
+
+        await Promise.all([...(orderCount === 1 ? [body.order] : body.order)].map(async (order, index) => {
+          if (order === 'text') {
+            return db.Text.create({
+              richtextId,
+              textHeading: Array.isArray(body.text_heading) ? body.text_heading.shift() : body.text_heading,
+              textDescription: Array.isArray(body.text_description) ? body.text_description.shift() : body.text_description,
+              order: index + 1
+            }, {
+              transaction
+            })
+          }
+
+          return db.Photo.create({
+            richtextId,
+            lessonImg: `courses/${files.courses.shift().filename}`,
+            photoDescription: Array.isArray(body.photo_description) ? body.photo_description.shift() : body.photo_description,
+            isImgDesc: false,
+            order: index + 1
+          }, {
+            transaction
+          })
+        }))
+      }
+
+      const createdCourse = await db.Courses.create({
+        ...courseData,
+        richtextId
+      }, {
+        transaction
+      })
+      createdCourseId = createdCourse.id
+    })
+
+    const course = await findCourseById(createdCourseId)
+
     res.status(201).json({
-      status: true,
-      message: ' new course added successfully',
-      data: course
+      message: 'New course added successfully',
+      data: course.get()
     })
   } catch (error) {
-    return res.json({ error: 'error.message' })
+    return res.status(400).json({ error: error.message })
   }
 }
 
@@ -113,80 +208,164 @@ const addCourse = async (req, res) => {
 // @route   PUT /api/courses/:id
 // @access  Public
 const updateCourse = async (req, res) => {
-  const { id } = req.params
   try {
-    let thumbnail
-    if (req.file) {
-      thumbnail = req.file.filename
+    const { body, user, files, params: { id: courseId } } = req
+
+    let course = await db.Courses.findByPk(courseId)
+
+    if (!course) {
+      throw new NotFoundError()
     }
-    const course = await db.Courses.update(
-      { ...req.body, thumbnail },
-      { where: { id } }
-    )
-    res.status(202).json({
-      status: true,
-      message: 'course updated successfully',
+
+    if (course.creatorId !== user.id) {
+      throw new ForbiddenRequestError()
+    }
+
+    const thumbnail = files.thumbnail?.[0]?.filename || null
+    const price = body.price > 0 ? Number(body.price) : null
+
+    const courseData = {
+      title: body.title,
+      description: body.description,
+      thumbnail: thumbnail ? `thumbnail/${thumbnail}` : null,
+      price,
+      isPublished: body.isPublished,
+      isFree: price === null ? true : false,
+      creatorId: user.id,
+      categoryId: body.categoryId
+    }
+
+    await db.sequelize.transaction(async (transaction) => {
+      let richtext = null
+      let richtextId = null
+
+      if (body.order) {
+        richtext = await db.RichText.create({}, { transaction })
+        richtextId = richtext.id
+
+        const richtextCount = Array.isArray(body.text_description) ? body.text_description.length : 1
+        const photoCount = files.courses?.length || 0
+        const orderCount = Array.isArray(body.order) ? body.order.length : 1
+
+        // add validation if texts and photo tallies the order count
+        if (photoCount + richtextCount !== orderCount) {
+          throw new BadRequestError('Contents don\'t match the ordering count')
+        }
+
+        await Promise.all([...(orderCount === 1 ? [body.order] : body.order)].map(async (order, index) => {
+          if (order === 'text') {
+            return db.Text.create({
+              richtextId,
+              textHeading: Array.isArray(body.text_heading) ? body.text_heading.shift() : body.text_heading,
+              textDescription: Array.isArray(body.text_description) ? body.text_description.shift() : body.text_description,
+              order: index + 1
+            }, {
+              transaction
+            })
+          }
+
+          return db.Photo.create({
+            richtextId,
+            lessonImg: `courses/${files.courses.shift().filename}`,
+            photoDescription: Array.isArray(body.photo_description) ? body.photo_description.shift() : body.photo_description,
+            isImgDesc: false,
+            order: index + 1
+          }, {
+            transaction
+          })
+        }))
+      }
+
+
+      await db.RichText.destroy({
+        where: {
+          id: course.richtextId
+        }
+      }, {
+        transaction
+      })
+      await course.update({
+        ...courseData,
+        richtextId
+      }, {
+        transaction
+      })
+
+    })
+
+    course = await findCourseById(courseId)
+
+    res.status(200).json({
+      message: 'Course updated successfully',
       data: course
     })
   } catch (error) {
-    res.json({ status: true, message: error.message })
+    res.status(400).json({ message: error.message })
   }
 }
+
+const findCourseById = (id) => db.Courses.findOne({
+  where: { id },
+  order: [[db.Lesson, 'createdAt', 'ASC']],
+  include: [
+    {
+      model: db.Lesson,
+      order: [['order', 'ASC']],
+      include: [db.Test, db.RichText,
+      // {
+      //   model: db.LessonProgress,
+      //   where: {
+      //     userId: {
+      //       [Op.eq]: req.user.id
+      //     }
+      //   },
+      //   required: false
+      // }
+      ]
+    },
+    {
+      model: db.User,
+      attributes: ['email'],
+      // where: {
+      //   id: {
+      //     [Op.eq]: req.user.id
+      //   }
+      // },
+      required: false,
+      as: 'enrolledUser',
+      through: {
+        attributes: ['isEnroll']
+      }
+    },
+    db.Category,
+    {
+      model: db.RichText,
+      include: [db.Text, db.Photo]
+    },
+  ],
+})
 
 // @desc    Fetch single course
 // @route   GET /api/courses/:id
 // @access  Public
 const getCourseById = async (req, res) => {
-  const course = await db.Courses.findOne({
-    where: { id: req.params.id },
-    order: [[db.Lesson, 'createdAt', 'ASC']],
-    include: [
-      {
-        model: db.Lesson,
-        order: [['order', 'ASC']],
-        include: [db.Test, db.RichText, {
-          model: db.LessonProgress,
-          where: {
-            userId: {
-              [Op.eq]: req.user.id
-            }
-          },
-          required: false
-        }]
-      },
-      {
-        model: db.User,
-        attributes: ['email'],
-        where: {
-          id: {
-            [Op.eq]: req.user.id
-          }
-        },
-        required: false,
-        as: 'enrolledUser',
-        through: {
-          attributes: ['isEnroll']
-        }
-      },
-      db.Category
-    ]
-  })
+  const { id } = req.params
+  const course = await findCourseById(id)
+
   if (!course) {
-    return res.json({ message: 'course not found' })
+    return res.status(404).json({ message: 'Course not found' })
   }
-  const data = Object.assign({
-    ...course,
-    dataValues: {
-      ...course.dataValues,
-      thumbnail: changeFormat(course?.dataValues?.thumbnail),
-      coverImg: changeFormat(course?.dataValues?.lessons?.coverImg)
-    }
-  })
-  const str = JSON.parse(CircularJSON.stringify(data))
+
+  const data = {
+    ...course.get(),
+    // TODO: need to fix resizing of image when used with multiple files
+    // thumbnail: changeFormat(course?.thumbnail),
+    // coverImg: changeFormat(course?.lessons?.coverImg)
+  }
+
   res.status(200).json({
-    status: true,
-    message: 'fetched course successfully',
-    data: str.dataValues
+    message: 'Course fetched successfully',
+    data
   })
 }
 
@@ -194,15 +373,27 @@ const getCourseById = async (req, res) => {
 // @route   delete /api/courses/:id
 // @access  Public
 const deleteCourse = async (req, res) => {
-  const { id } = req.params
-  const course = await db.Courses.destroy({ where: { id } })
-  if (!course) {
-    throw new NotFoundError()
-  }
-  res.status(202).json({
-    status: true,
-    message: 'course deleted successfully',
-    data: course
+  const { params: { id: courseId }, user } = req
+
+  await db.sequelize.transaction(async (transaction) => {
+    const course = await db.Courses.findByPk(courseId)
+
+    if (!course) {
+      throw new NotFoundError()
+    }
+
+    if (course.creatorId !== user.id) {
+      throw new ForbiddenRequestError()
+    }
+
+    await course.destroy({ transaction })
+    await db.RichText.destroy({
+      where: { id: course.richtextId }
+    })
+  })
+
+  res.status(200).json({
+    message: 'Course deleted successfully'
   })
 }
 
@@ -211,5 +402,6 @@ module.exports = {
   getCourses,
   updateCourse,
   getCourseById,
-  deleteCourse
+  deleteCourse,
+  courseSchema
 }
---
+++ api/src/middleware/validation.js
@@ -0,0 +1,10 @@
+const { validate } = require('express-validation')
+
+const validation = (schema) =>
+  validate(
+    schema,
+    { keyByField: true },
+    { abortEarly: false }
+  )
+
+module.exports = validation
